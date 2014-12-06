6f0d3e7a009c9ba4c959f2b79e772261b69d5e9e
hellochar <hellocharlien@hotmail.com>
2014-12-06 15:40:02 -0800

    better sketch1

--- public/javascripts/sketch1.js
+++ public/javascripts/sketch1.js
@@ -1,93 +1,173 @@
 (function () {
-    function sketch1(canvas, context) {
-      var mapping = {
-        'd': function(state) {
-          var dx = Math.cos(state.angle),
-              dy = Math.sin(state.angle);
-          var oldX = state.x;
-          var oldY = state.y;
-
-          state.x += dx * 10;
-          state.y += dy * 10;
-
-          context.beginPath();
-          context.moveTo(oldX, oldY);
-          context.lineTo(state.x, state.y);
-          context.stroke();
-        },
-        'm': function(state) {
-          var dx = Math.cos(state.angle),
-              dy = Math.sin(state.angle);
-          state.x += dx * 10;
-          state.y += dy * 10;
-        },
-        'l': function(state) {
-          var rand = 1 + Math.random() / 4;
-          state.angle += Math.PI / 16 * rand;
-        },
-        'r': function(state) {
-          var rand = 1 + Math.random() / 4;
-          state.angle -= Math.PI / 16 * rand;
-        },
-        '[': function(state) {
-          state.stack.push({x: state.x, y : state.y, angle: state.angle });
-        },
-        ']': function(state) {
-          var lastState = state.stack.pop();
-          state.x = lastState.x;
-          state.y = lastState.y;
-          state.angle = lastState.angle;
-        }
-      }
-
-      window.context = context;
-
-      function execute(program, mapping) {
+    /**
+     * Execute the program for the given mapping beginning with an initial state.
+     *
+     * @param {string} program - the string of instructions (each a single character) to take
+     * @param {char -> (State) => State) mapping - the mapping from instruction -> function that transforms state and possibly has side effects such as drawing
+     * @param {2DRenderingContext} context - canvas context for drawing
+     */
+    function execute(program, mapping, context) {
         var state = {
-          x: 0,
-          y: 0,
-          angle: 0,
-          stack: []
+            x: 0,
+            y: 0,
+            angle: 0,
+            stack: []
         };
         program.split("").forEach(function (instruction) {
-          var fn = mapping[instruction];
-          fn && fn(state);
+            var fn = mapping[instruction];
+            fn && fn(state, context);
         });
-      }
+    }
 
-      function transform(program, transformationRules) {
-        return program.split("").map(function (instruction) {
-          return transformationRules[instruction] || instruction;
-        }).join("");
-      }
+    /**
+     * @return {Function () => Boolean} a function that, when called, will run execute's for loop one more time. It returns true when done, or false if it has more to go.
+     */
+    function createExecuteGenerator(program, mapping, context) {
+        var state = {
+            x: 0,
+            y: 0,
+            angle: 0,
+            stack: []
+        };
 
-      var program = "a";
-      var transformationRules = {
-        'a': 'd[rrdalllla]'
-      }
-
-      function animate() {
-        var millisBefore = (new Date()).getTime();
-        context.resetTransform();
-        context.save();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.translate(canvas.width/2, canvas.height/2);
-        context.strokeStyle = "#1498cc";
-        execute(program, mapping);
-        context.restore();
-        context.font = "20px Arial";
-        context.fillStyle = "black";
-        context.fillText(program, 0, 20);
-        program = transform(program, transformationRules);
-        var millisAfter = (new Date()).getTime();
-        var elapsed = millisAfter - millisBefore;
-        console.log(elapsed)
-        if (elapsed < 100) {
-          setTimeout(animate, 1000);
+        function runInstruction() {
+            if (runInstruction.index < runInstruction.program.length) {
+                var instruction = runInstruction.program.charAt(runInstruction.index);
+                var fn = mapping[instruction];
+                fn && fn(state, context);
+                runInstruction.index += 1;
+                if (runInstruction.index < runInstruction.program.length) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return true;
+            }
         }
-      }
+        runInstruction.index = 0;
+        runInstruction.program = program;
+        return runInstruction;
+    };
 
-      animate();
+    /**
+     * Transform the program with the given rules.
+     *
+     * @param {string} program - the string of instructions to transform
+     * @param {char -> string} transformationRules - a mapping from instruction -> string that the instruction expands into
+     * @returns {string} a new program where each transformed instruction from the original program is concatenated together.
+     */
+    function transform(program, transformationRules) {
+        return program.split("").map(function (instruction) {
+            return transformationRules[instruction] || instruction;
+        }).join("");
+    }
+
+    function sketch1(canvas, context) {
+        var program = "a";
+        var transformationRules = {
+            'a': 'd[rrdablllla]',
+            'b': 'drdblda'
+        }
+        var mapping = {
+            'd': function(state, context) {
+                var dx = Math.cos(state.angle),
+                    dy = Math.sin(state.angle);
+                var oldX = state.x;
+                var oldY = state.y;
+
+                state.x += dx * 10;
+                state.y += dy * 10;
+
+                context.strokeStyle = "#1498cc";
+                context.beginPath();
+                context.moveTo(oldX, oldY);
+                context.lineTo(state.x, state.y);
+                context.stroke();
+            },
+            'm': function(state) {
+                var dx = Math.cos(state.angle),
+                    dy = Math.sin(state.angle);
+                state.x += dx * 10;
+                state.y += dy * 10;
+            },
+            'l': function(state) {
+                state.angle += Math.PI / 16;
+            },
+            'r': function(state) {
+                state.angle -= Math.PI / 16;
+            },
+            '[': function(state) {
+                state.stack.push({x: state.x, y : state.y, angle: state.angle });
+            },
+            ']': function(state) {
+                var lastState = state.stack.pop();
+                state.x = lastState.x;
+                state.y = lastState.y;
+                state.angle = lastState.angle;
+            }
+        }
+
+
+        var programDisplayElement = $('<div class="program-display"></div>');
+        $(canvas).parent().append(programDisplayElement);
+
+        var MILLIS_PER_ITERATION = 1000;
+
+        var executeFn;
+
+        function setExecuteFn() {
+            context.resetTransform();
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.translate(canvas.width/2, canvas.height/2);
+            executeFn = createExecuteGenerator(program, mapping, context);
+            executeFn.startTime = (new Date()).getTime();
+            MILLIS_PER_ITERATION += 1000;
+        }
+
+        setExecuteFn();
+
+        function animate() {
+            var timeElapsed = (new Date()).getTime() - executeFn.startTime;
+            var wantedIndex = Math.floor(Math.min(timeElapsed / MILLIS_PER_ITERATION, 1) * program.length);
+            var done = false;
+            while (executeFn.index < wantedIndex && !done) {
+                done = executeFn();
+            }
+
+            var prefix = program.substring(0, executeFn.index);
+            var highlightedChar = program.charAt(executeFn.index);
+            var suffix = program.substring(executeFn.index + 1);
+            programDisplayElement.html(prefix+'<span class="highlighted">'+highlightedChar+'</span>'+suffix);
+            if (done) {
+                program = transform(program, transformationRules);
+                setTimeout(function () {
+                    setExecuteFn();
+                    animate();
+                }, 1000);
+            } else {
+                requestAnimationFrame(animate);
+            }
+        }
+        requestAnimationFrame(animate);
+
+        // function animate() {
+        //     var millisBefore = (new Date()).getTime();
+        //     context.resetTransform();
+        //     context.clearRect(0, 0, canvas.width, canvas.height);
+        //     context.translate(canvas.width/2, canvas.height/2);
+        //     
+        //     execute(program, mapping, context);
+        //     var millisAfter = (new Date()).getTime();
+        //     var elapsed = millisAfter - millisBefore;
+        //     if (elapsed < 20) {
+        //         programDisplayElement.text(program);
+        //         program = transform(program, transformationRules);
+        //     }
+        //     setTimeout(animate, 1000);
+        // }
+
+        // animate();
     }
 
     initializeSketch(sketch1, "sketch1");
