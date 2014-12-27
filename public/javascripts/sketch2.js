9e9891d866a98d123ae47f3105f943fd1c4470d7
Xiaohan Zhang <hellocharlien@hotmail.com>
2014-12-26 17:17:19 -0700

    update sketch2 to work with new html

--- public/javascripts/sketch2.js
+++ public/javascripts/sketch2.js
@@ -2,7 +2,10 @@
     var NUM_PARTICLES = 20000;
     var TIME_STEP = 1 / 20;
     var GRAVITY_CONSTANT = 100;
-    var DRAG_CONSTANT = 0.995;
+    var PULLING_DRAG_CONSTANT = 0.998;
+    var INERTIAL_DRAG_CONSTANT = 0.982;
+
+    var dragConstant = INERTIAL_DRAG_CONSTANT;
 
     var returnToStartPower = 0;
 
@@ -11,82 +14,92 @@
     };
     var particles = [];
 
-    var sketch2 = {
-        init: function (canvas, context) {
-            var resetButton = $('<button class="reset">Reset</button>').click(function() {
-                for (var i = 0; i < NUM_PARTICLES; i++) {
-                    particles[i].dx = 0;
-                    particles[i].dy = 0;
-                }
-                returnToStartPower = 0.01;
-            });
-            $(canvas).parent().append(resetButton);
-            $(canvas).mousedown(function (event) {
-                attractors["mouse"] = { x: event.offsetX, y : event.offsetY };
-                returnToStartPower = 0;
-            }).mousemove(function (event) {
-                if (attractors["mouse"]) {
-                    attractors["mouse"].x = event.offsetX;
-                    attractors["mouse"].y = event.offsetY;
-                }
-            }).mouseup(function (event) {
-                attractors["mouse"] = null;
-            });
+    var html = '<canvas></canvas><button class="reset">Reset</button>';
 
-            for( var i = 0; i < NUM_PARTICLES; i++ ) {
-                particles[i] = {
-                    x: i * canvas.width / NUM_PARTICLES,
-                    y: canvas.height / 2,
-                    dx: 0,
-                    dy: 0
-                };
-            }
-        },
+    var canvas;
 
-        animate: function (canvas, context) {
-            var start = (new Date()).getTime();
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            context.strokeStyle = "white";
-            context.beginPath();
-
-            if (returnToStartPower > 0 && returnToStartPower < 1) {
-                returnToStartPower *= 1.01;
-            }
-
+    function init($sketchElement, context) {
+        canvas = $sketchElement.find("canvas")[0];
+        $sketchElement.find(".reset").click(function() {
             for (var i = 0; i < NUM_PARTICLES; i++) {
-                var particle = particles[i];
-                for (var attractorName in attractors) {
-                    var attractor = attractors[attractorName];
-                    if (attractor == null) continue;
-                    var dx = attractor.x - particle.x;
-                    var dy = attractor.y - particle.y;
-                    var length2 = Math.sqrt(dx*dx + dy*dy);
-                    var forceX = GRAVITY_CONSTANT * dx / length2;
-                    var forceY = GRAVITY_CONSTANT * dy / length2;
-
-                    particle.dx += forceX * TIME_STEP;
-                    particle.dy += forceY * TIME_STEP;
-                }
-                particle.dx *= DRAG_CONSTANT;
-                particle.dy *= DRAG_CONSTANT;
-
-                particle.x += particle.dx * TIME_STEP;
-                particle.y += particle.dy * TIME_STEP;
-
-                var wantedX = i * canvas.width / NUM_PARTICLES;
-                var wantedY = canvas.height / 2;
-                if (returnToStartPower > 0) {
-                    particle.x -= (particle.x - wantedX) * returnToStartPower;
-                    particle.y -= (particle.y - wantedY) * returnToStartPower;
-                }
-
-                context.moveTo(particle.x, particle.y);
-                context.lineTo(particle.x + 1, particle.y);
+                particles[i].dx = 0;
+                particles[i].dy = 0;
             }
-            context.stroke();
-            var elapsed = (new Date()).getTime() - start;
-            console.log(1000 / elapsed);
+            returnToStartPower = 0.01;
+        });
+        $(canvas).mousedown(function (event) {
+            attractors["mouse"] = { x: event.offsetX, y : event.offsetY };
+            dragConstant = PULLING_DRAG_CONSTANT;
+            returnToStartPower = 0;
+        }).mousemove(function (event) {
+            if (attractors["mouse"]) {
+                attractors["mouse"].x = event.offsetX;
+                attractors["mouse"].y = event.offsetY;
+            }
+        }).mouseup(function (event) {
+            dragConstant = INERTIAL_DRAG_CONSTANT;
+            attractors["mouse"] = null;
+        });
+
+        for( var i = 0; i < NUM_PARTICLES; i++ ) {
+            particles[i] = {
+                x: i * canvas.width / NUM_PARTICLES,
+                y: canvas.height / 2,
+                dx: 0,
+                dy: 0
+            };
         }
+    }
+
+    function animate($sketchElement, context) {
+        var start = (new Date()).getTime();
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.strokeStyle = "white";
+        context.beginPath();
+
+        if (returnToStartPower > 0 && returnToStartPower < 1) {
+            returnToStartPower *= 1.01;
+        }
+
+        for (var i = 0; i < NUM_PARTICLES; i++) {
+            var particle = particles[i];
+            for (var attractorName in attractors) {
+                var attractor = attractors[attractorName];
+                if (attractor == null) continue;
+                var dx = attractor.x - particle.x;
+                var dy = attractor.y - particle.y;
+                var length2 = Math.sqrt(dx*dx + dy*dy);
+                var forceX = GRAVITY_CONSTANT * dx / length2;
+                var forceY = GRAVITY_CONSTANT * dy / length2;
+
+                particle.dx += forceX * TIME_STEP;
+                particle.dy += forceY * TIME_STEP;
+            }
+            particle.dx *= dragConstant;
+            particle.dy *= dragConstant;
+
+            particle.x += particle.dx * TIME_STEP;
+            particle.y += particle.dy * TIME_STEP;
+
+            var wantedX = i * canvas.width / NUM_PARTICLES;
+            var wantedY = canvas.height / 2;
+            if (returnToStartPower > 0) {
+                particle.x -= (particle.x - wantedX) * returnToStartPower;
+                particle.y -= (particle.y - wantedY) * returnToStartPower;
+            }
+
+            context.moveTo(particle.x, particle.y);
+            context.lineTo(particle.x + 1, particle.y);
+        }
+        context.stroke();
+        var elapsed = (new Date()).getTime() - start;
+        console.log(1000 / elapsed);
+    }
+
+    var sketch2 = {
+        init: init,
+        animate: animate,
+        html: html
     };
     initializeSketch(sketch2, "sketch2");
 })();
