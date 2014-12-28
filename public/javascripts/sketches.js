150721afe2325cf9c1a1d722e096f81795a43a2f
Xiaohan Zhang <hellocharlien@hotmail.com>
2014-12-28 16:03:42 -0700

    add pixi.js to bower components; reformat sketches.js

--- public/javascripts/sketches.js
+++ public/javascripts/sketches.js
@@ -22,53 +22,54 @@
     }
 
     function initializeSketch(sketchObj, sketchId) {
-      var init = sketchObj.init;
-      var animate = sketchObj.animate;
-      var sketchHtml = sketchObj.html || DEFAULT_SKETCH_HTML;
+        var init = sketchObj.init;
+        var animate = sketchObj.animate;
+        var sketchHtml = sketchObj.html || DEFAULT_SKETCH_HTML;
 
-      // add sketch element to nav
-      var $navElement = $('<li></li>');
-      $navElement.text(sketchId)
-                 .click(function () {
-                     $('html, body').animate({ scrollTop: $sketchElement.offset().top }, 600);
-                 })
-                 .appendTo($navbarElement);
+        // add sketch element to nav
+        var $navElement = $('<li></li>');
+        $navElement
+            .text(sketchId)
+            .click(function () {
+                $('html, body').animate({ scrollTop: $sketchElement.offset().top }, 600);
+            })
+            .appendTo($navbarElement);
 
-      // add sketch element to body
-      var $sketchElement = $('<div></div>').addClass("sketch-wrapper").attr('id', sketchId);
-      $allSketches.append($sketchElement);
-      $sketchElement.append(sketchHtml);
+        // add sketch element to body
+        var $sketchElement = $('<div></div>').addClass("sketch-wrapper").attr('id', sketchId);
+        $allSketches.append($sketchElement);
+        $sketchElement.append(sketchHtml);
 
-      // canvas setup
-      var $canvas = $sketchElement.find("canvas");
-      ["mousedown", "mouseup", "mousemove"].forEach(function (eventName) {
-          if (sketchObj[eventName] != null) {
-              $canvas[eventName](sketchObj[eventName]);
-          }
-      });
+        // canvas setup
+        var $canvas = $sketchElement.find("canvas");
+        ["mousedown", "mouseup", "mousemove"].forEach(function (eventName) {
+            if (sketchObj[eventName] != null) {
+                $canvas[eventName](sketchObj[eventName]);
+            }
+        });
 
-      setCanvasDimensions($canvas);
-      $window.resize(function() {
-          setCanvasDimensions($canvas);
-      });
+        setCanvasDimensions($canvas);
+        $window.resize(function() {
+            setCanvasDimensions($canvas);
+        });
 
-      // initialize and run sketch
-      var lastAnimate = (new Date()).getTime();
-      function animateAndRequestAnimationFrame() {
-          if (isElementOnScreen($sketchElement)) {
-              $sketchElement.removeClass("disabled");
-              animate($sketchElement, $canvas[0].getContext('2d'));
-              var now = (new Date()).getTime();
-              var elapsed = now - lastAnimate;
-              lastAnimate = now;
-              console.log(sketchId, 1000 / elapsed);
-          } else {
-              $sketchElement.addClass("disabled");
-          }
-          requestAnimationFrame(animateAndRequestAnimationFrame);
-      }
-      init($sketchElement, $canvas[0].getContext('2d'));
-      animateAndRequestAnimationFrame();
+        // initialize and run sketch
+        var lastAnimate = (new Date()).getTime();
+        function animateAndRequestAnimationFrame() {
+            if (isElementOnScreen($sketchElement)) {
+                $sketchElement.removeClass("disabled");
+                animate($sketchElement, $canvas[0].getContext('2d'));
+                var now = (new Date()).getTime();
+                var elapsed = now - lastAnimate;
+                lastAnimate = now;
+                console.log(sketchId, 1000 / elapsed);
+            } else {
+                $sketchElement.addClass("disabled");
+            }
+            requestAnimationFrame(animateAndRequestAnimationFrame);
+        }
+        init($sketchElement, $canvas[0].getContext('2d'));
+        animateAndRequestAnimationFrame();
     }
 
     window.initializeSketch = initializeSketch;
