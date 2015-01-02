321a2ab59ff58c867ad70629d6695f2e8358a8e0
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-01-02 01:45:42 -0700

    add bluebird library

---
+++ bower_components/bluebird/src/props.js
@@ -0,0 +1,89 @@
+"use strict";
+module.exports = function(Promise, PromiseArray, tryConvertToPromise) {
+var ASSERT = require("./assert.js");
+var util = require("./util.js");
+var apiRejection = require("./errors_api_rejection")(Promise);
+var isObject = util.isObject;
+var es5 = require("./es5.js");
+
+function PropertiesPromiseArray(obj) {
+    var keys = es5.keys(obj);
+    var len = keys.length;
+    var values = new Array(len * 2);
+    for (var i = 0; i < len; ++i) {
+        var key = keys[i];
+        values[i] = obj[key];
+        values[i + len] = key;
+    }
+    this.constructor$(values);
+}
+util.inherits(PropertiesPromiseArray, PromiseArray);
+
+//Override
+PropertiesPromiseArray.prototype._init = function () {
+    this._init$(undefined, RESOLVE_OBJECT) ;
+};
+
+//Override
+PropertiesPromiseArray.prototype._promiseFulfilled = function (value, index) {
+    ASSERT(!this._isResolved());
+    ASSERT(!(value instanceof Promise));
+    this._values[index] = value;
+    var totalResolved = ++this._totalResolved;
+    if (totalResolved >= this._length) {
+        var val = {};
+        var keyOffset = this.length();
+        for (var i = 0, len = this.length(); i < len; ++i) {
+            val[this._values[i + keyOffset]] = this._values[i];
+        }
+        this._resolve(val);
+    }
+};
+
+//Override
+PropertiesPromiseArray.prototype._promiseProgressed = function (value, index) {
+    ASSERT(!this._isResolved());
+
+    this._promise._progress({
+        key: this._values[index + this.length()],
+        value: value
+    });
+};
+
+// Override
+PropertiesPromiseArray.prototype.shouldCopyValues = function () {
+    return false;
+};
+
+// Override
+PropertiesPromiseArray.prototype.getActualLength = function (len) {
+    return len >> 1;
+};
+
+function props(promises) {
+    var ret;
+    var castValue = tryConvertToPromise(promises, undefined);
+
+    if (!isObject(castValue)) {
+        return apiRejection(PROPS_TYPE_ERROR);
+    } else if (castValue instanceof Promise) {
+        ret = castValue._then(
+            Promise.props, undefined, undefined, undefined, undefined);
+    } else {
+        ret = new PropertiesPromiseArray(castValue).promise();
+    }
+
+    if (castValue instanceof Promise) {
+        ret._propagateFrom(castValue, PROPAGATE_BIND);
+    }
+    return ret;
+}
+
+Promise.prototype.props = function () {
+    return props(this);
+};
+
+Promise.props = function (promises) {
+    return props(promises);
+};
+};
---
+++ bower_components/bluebird/src/timers.js
@@ -0,0 +1,81 @@
+"use strict";
+var ASSERT = require("./assert.js");
+module.exports = function(Promise, INTERNAL, tryConvertToPromise) {
+var util = require("./util.js");
+var errors = require("./errors.js");
+var apiRejection = require("./errors_api_rejection")(Promise);
+var TimeoutError = Promise.TimeoutError;
+
+var afterTimeout = function (promise, message) {
+    //Don't waste time concatting strings or creating stack traces
+    if (!promise.isPending()) return;
+    if (typeof message !== "string") {
+        message = TIMEOUT_ERROR;
+    }
+    var err = new TimeoutError(message);
+    errors.markAsOriginatingFromRejection(err);
+    promise._attachExtraTrace(err);
+    promise._cancel(err);
+};
+
+var afterDelay = function (value, promise) {
+    promise._fulfill(value);
+};
+
+var delay = Promise.delay = function (value, ms) {
+    if (ms === undefined) {
+        ms = value;
+        value = undefined;
+    }
+    ms = +ms;
+    var maybePromise = tryConvertToPromise(value, undefined);
+    var promise = new Promise(INTERNAL);
+
+    if (maybePromise instanceof Promise) {
+        promise._propagateFrom(maybePromise, PROPAGATE_ALL);
+        promise._follow(maybePromise._target());
+        return promise.then(function(value) {
+            return Promise.delay(value, ms);
+        });
+    } else {
+        promise._setTrace(undefined);
+        setTimeout(function delayTimeout() {
+            afterDelay(value, promise);
+        }, ms);
+    }
+    return promise;
+};
+
+Promise.prototype.delay = function (ms) {
+    return delay(this, ms);
+};
+
+function successClear(value) {
+    var handle = this;
+    // Deal with non-strict mode wrapping.
+    if (handle instanceof Number) handle = +handle;
+    clearTimeout(handle);
+    return value;
+}
+
+function failureClear(reason) {
+    var handle = this;
+    // Deal with non-strict mode wrapping.
+    if (handle instanceof Number) handle = +handle;
+    clearTimeout(handle);
+    throw reason;
+}
+
+Promise.prototype.timeout = function (ms, message) {
+    var target = this._target();
+    ms = +ms;
+    var ret = new Promise(INTERNAL).cancellable();
+    ret._propagateFrom(this, PROPAGATE_ALL);
+    ret._follow(target);
+    var handle = setTimeout(function timeoutTimeout() {
+        afterTimeout(ret, message);
+    }, ms);
+    return ret._then(successClear, failureClear, undefined, handle, undefined);
+};
+
+};
