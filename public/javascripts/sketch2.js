ac94adeef742c0420b76b9bf30f263c7f0a84bc4
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-01-02 01:45:50 -0700

    common math library; sketch2 changes
    sketch2 has basic audio reactivity - need to play around with the sound.
    Also has an auto-attract feature

--- public/javascripts/sketch2.js
+++ public/javascripts/sketch2.js
@@ -1,22 +1,120 @@
 (function () {
-    var NUM_PARTICLES = 15000;
+
+    var NUM_PARTICLES = 10000;
     var TIME_STEP = 1 / 20;
     var GRAVITY_CONSTANT = 100;
-
     // speed becomes this percentage of its original speed every second
     var PULLING_DRAG_CONSTANT = 0.96075095702;
     var INERTIAL_DRAG_CONSTANT = 0.73913643334;
 
+    var attractor = null;
+    var canvas;
     var dragConstant = INERTIAL_DRAG_CONSTANT;
-
+    var lastAutoAttractPromise;
+    var particles = [];
     var returnToStartPower = 0;
 
-    var attractor = null;
-    var particles = [];
+    function beginAutoAttract() {
+        if (lastAutoAttractPromise != null) {
+            endAutoAttract();
+        }
+        function createAutoAttractLoop() {
+            var iteration = P.delay(8000).cancellable()
+            .then(function () {
+                attractor = {
+                    x: Math.map(Math.random(), 0, 1, canvas.width / 4, canvas.width * 3 / 4),
+                    y: Math.map(Math.random(), 0, 1, canvas.height / 4, canvas.height * 3 / 4)
+                }
+            }).then(function () {
+                return P.delay(3000);
+            }).then(function () {
+                attractor = null;
+            }).then(function () {
+                return createAutoAttractLoop();
+            });
+            return iteration;
+        }
 
-    var html = '<div class="topbar">Click for gravity.<button class="reset">Reset</button></div><canvas></canvas>';
+        lastAutoAttractPromise = P.delay(5000).cancellable().then(function () { return createAutoAttractLoop(); });
+        return lastAutoAttractPromise;
+    }
 
-    var canvas;
+    function endAutoAttract() {
+        try {
+            lastAutoAttractPromise.cancel();
+        } catch(e) {
+            // no op
+        }
+        lastAutoAttractPromise = null;
+    }
+
+    var AUDIO_PARTICLE_SAMPLE_COUNT = 20;
+    var AUDIO_FAST_VELOCITY = 200;
+
+    var audioContext = new AudioContext();
+    var audioGroups = [];
+
+    function createAudioGroup() {
+        var noiseGain = audioContext.createGain();
+        noiseGain.gain.value = 0.0;
+
+        // var noise = (function() {
+        //     var node = audioContext.createBufferSource()
+        //     , buffer = audioContext.createBuffer(1, audioContext.sampleRate * 5, audioContext.sampleRate)
+        //     , data = buffer.getChannelData(0);
+        //
+        //     for (var i = 0; i < buffer.length; i++) {
+        //         data[i] = Math.random();
+        //     }
+        //     node.buffer = buffer;
+        //     node.loop = true;
+        //     return node;
+        // })();
+        //
+
+        // pink noise from http://noisehack.com/generate-noise-web-audio-api/
+        var noise = (function() {
+            var bufferSize = 4096;
+            var b0, b1, b2, b3, b4, b5, b6;
+            b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+            var node = audioContext.createScriptProcessor(bufferSize, 1, 1);
+            node.onaudioprocess = function(e) {
+                var output = e.outputBuffer.getChannelData(0);
+                for (var i = 0; i < bufferSize; i++) {
+                    var white = Math.random() * 2 - 1;
+                    b0 = 0.99886 * b0 + white * 0.0555179;
+                    b1 = 0.99332 * b1 + white * 0.0750759;
+                    b2 = 0.96900 * b2 + white * 0.1538520;
+                    b3 = 0.86650 * b3 + white * 0.3104856;
+                    b4 = 0.55000 * b4 + white * 0.5329522;
+                    b5 = -0.7616 * b5 - white * 0.0168980;
+                    output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+                    output[i] *= 0.11; // (roughly) compensate for gain
+                    b6 = white * 0.115926;
+                }
+            }
+            return node;
+        })();
+
+        var lowpass = audioContext.createBiquadFilter();
+        lowpass.type = "lowpass";
+        lowpass.frequency.value = 1000;
+        lowpass.Q.value = 2.5;
+
+
+        noise.connect(noiseGain);
+        noiseGain.connect(lowpass);
+        return {
+            noise: noise,
+            noiseGain: noiseGain,
+            lowpass: lowpass
+        };
+    }
+
+    for(var i = 0; i < AUDIO_PARTICLE_SAMPLE_COUNT; i++) {
+        audioGroups[i] = createAudioGroup();
+        audioGroups[i].lowpass.connect(audioContext.destination);
+    }
 
     function init($sketchElement, stage, renderer) {
         canvas = $sketchElement.find("canvas")[0];
@@ -51,13 +149,14 @@
                 sprite: particleSprite
             };
         }
+
+        beginAutoAttract();
     }
 
     function animate($sketchElement, stage, renderer) {
         if (returnToStartPower > 0 && returnToStartPower < 1) {
             returnToStartPower *= 1.01;
         }
-
         for (var i = 0; i < NUM_PARTICLES; i++) {
             var particle = particles[i];
             if (attractor != null) {
@@ -86,6 +185,15 @@
             particle.sprite.position.x = particle.x;
             particle.sprite.position.y = particle.y;
         }
+
+        for (var i = 0; i < AUDIO_PARTICLE_SAMPLE_COUNT; i++) {
+            var audioGroup = audioGroups[i];
+            var particle = particles[Math.floor(Math.map(i, 0, AUDIO_PARTICLE_SAMPLE_COUNT - 1, 0, NUM_PARTICLES - 1))];
+            var velocity = Math.sqrt(particle.dx * particle.dx + particle.dy * particle.dy);
+            var wantedGain = Math.map(velocity, 0, AUDIO_FAST_VELOCITY, 0, 1) / AUDIO_PARTICLE_SAMPLE_COUNT;
+            audioGroup.lowpass.frequency.value = Math.map(particle.y, 0, canvas.height, 300, 15000);
+            audioGroup.noiseGain.gain.value = wantedGain;
+        }
         renderer.render(stage);
     }
 
@@ -95,6 +203,7 @@
         attractor = { x: mouseX, y : mouseY };
         dragConstant = PULLING_DRAG_CONSTANT;
         returnToStartPower = 0;
+        endAutoAttract();
     }
 
     function mousemove(event) {
@@ -104,17 +213,19 @@
             attractor.x = mouseX;
             attractor.y = mouseY;
         }
+        endAutoAttract();
     }
 
     function mouseup(event) {
         dragConstant = INERTIAL_DRAG_CONSTANT;
         attractor = null;
+        beginAutoAttract();
     }
 
     var sketch2 = {
         init: init,
         animate: animate,
-        html: html,
+        html: '<div class="topbar">Click for gravity.<button class="reset">Reset</button></div><canvas></canvas>',
         mousedown: mousedown,
         mousemove: mousemove,
         mouseup: mouseup,
