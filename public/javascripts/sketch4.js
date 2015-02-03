6bfa9188a74c9a4f0a1522db99539e1798797600
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-02-03 03:14:01 -0800

    remove console.log

--- public/javascripts/sketch4.js
+++ public/javascripts/sketch4.js
@@ -358,7 +358,6 @@
     }
 
     function resize(width, height) {
-        console.log("resizing to", width, height);
         camera.left = -width/2;
         camera.right = width/2;
         camera.top = -height/2;
