d17babf811e869b7de6bff44051feec944dc9386
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-02-15 18:02:20 -0800

    HeightMap object to namespace functions; simple audio

--- public/javascripts/sketch3.js
+++ public/javascripts/sketch3.js
@@ -1,31 +1,39 @@
 (function () {
-    var frame = 0;
-    var canvas;
+    var HeightMap = {
+        canvasWidth: null,
+        canvasHeight: null,
+        frame: 0,
+        /**
+         * How wavy the heightmap is, from [0..1]. 0 means not wavy at all (only bulbous); 1.0 means only wavy.
+         */
+        getWaviness: function() {
+            return (1+Math.sin(HeightMap.frame / 100))/2;
+        },
+        evaluate: function(x, y) {
+            var dx = (x - HeightMap.canvasWidth/2);
+            var dy = (y - HeightMap.canvasHeight/2);
+            var length2 = dx*dx + dy*dy;
+            // z1 creates the bulb shape at the center (using a logistic function)
+            var z1 = 23000 / (1 + Math.exp(-length2 / 10000));
+            // z2 creates the radial wave shapes from the center
+            var z2 = 600 * Math.cos(length2 / 25000 + HeightMap.frame / 25);
+            // z3 is a smaller radial wave shape that is centered towards the mouse
+            var z3 = 100 * Math.cos(Math.sqrt(Math.pow(x - mouseX, 2) + Math.pow(y - mouseY, 2)) / 20 + HeightMap.frame / 25);
 
-    var canvasWidth, canvasHeight;
+            return Math.lerp(z1, z2, HeightMap.getWaviness()) + z3;
+        },
+        gradient: function (x,y) {
+            var fnxy = HeightMap.evaluate(x, y);
+            var epsilon = 1e-4;
+            var ddx = (HeightMap.evaluate(x + epsilon, y) - fnxy) / epsilon;
+            var ddy = (HeightMap.evaluate(x, y + epsilon) - fnxy) / epsilon;
+
+            return [ddx, ddy];
+        }
+    }
 
     var mouseX = 0;
     var mouseY = 0;
-
-    function fn(x, y) {
-        var dx = (x - canvasWidth/2);
-        var dy = (y - canvasHeight/2);
-        var length2 = dx*dx + dy*dy;
-        var z1 = 23000 / (1 + Math.exp(-length2 / 10000));
-        var z2 = 600 * Math.cos(length2 / 25000 + frame / 25);
-        var z3 = 40 * Math.cos(Math.sqrt(Math.pow(x - mouseX, 2) + Math.pow(y - mouseY, 2)) / 20 + frame / 25);
-
-        return Math.lerp(z1, z2, (1+Math.sin(frame / 100))/2) + z3;
-    }
-
-    function gradient(x, y) {
-        var fnxy = fn(x, y);
-        var epsilon = 1e-4;
-        var ddx = (fn(x + epsilon, y) - fnxy) / epsilon;
-        var ddy = (fn(x, y + epsilon) - fnxy) / epsilon;
-
-        return [ddx, ddy];
-    }
 
     function permutedLine(ox, oy, nx, ny, geometry) {
         var distance = Math.sqrt(Math.pow(ox-nx, 2) + Math.pow(oy-ny, 2));
@@ -38,7 +46,7 @@
         }
 
         function permutePoint(x, y, idx) {
-            var grad = gradient(x, y);
+            var grad = HeightMap.gradient(x, y);
             geometry.vertices[idx].set(x + grad[0], y + grad[1], 0);
         }
 
@@ -48,10 +56,6 @@
                          oy + (ny - oy) * percentage, t);
         }
         return geometry;
-    }
-
-    function signum(x) {
-        return x / Math.abs(x);
     }
 
     // offsetX and offsetY define the vector that the line draws on (the inline direction). the direction that
@@ -70,9 +74,9 @@
     }
 
     LineStrip.prototype.update = function(dx, dy) {
-        this.gridOffsetX = (this.gridOffsetX + dx) % this.gridSize;
-        this.gridOffsetY = (this.gridOffsetY + dy) % this.gridSize;
-        console.log(this.gridOffsetX, this.gridOffsetY);
+        this.gridOffsetX = ((this.gridOffsetX + dx) % this.gridSize + this.gridSize) % this.gridSize;
+        this.gridOffsetY = ((this.gridOffsetY + dy) % this.gridSize + this.gridSize) % this.gridSize;
+        // console.log(this.gridOffsetX, this.gridOffsetY);
         this.object.children.forEach(function(lineMesh) {
             var x = lineMesh.x;
             var y = lineMesh.y;
@@ -131,37 +135,37 @@
             , buffer = audioContext.createBuffer(1, audioContext.sampleRate * 5, audioContext.sampleRate)
             , data = buffer.getChannelData(0);
             for (var i = 0; i < buffer.length; i++) {
-                data[i] = Math.random();
+                data[i] = Math.random() * 2 - 1;
             }
             node.buffer = buffer;
             node.loop = true;
             node.start(0);
             return node;
         })();
-        var noiseGain = audioContext.createGain();
-        noiseGain.gain.value = 0.05;
-        noise.connect(noiseGain);
 
         function setBiquadParameters(node, time) {
-            node.a0 = Math.map(Math.sin(time * 1.1 * Math.PI * 2), -1, 1, 0.3, 1.0);
-            node.a1 = -0.0;
+            // node.a0 = Math.map(Math.sin(time * 1.1 * Math.PI * 2), -1, 1, 0.3, 1.0);
+            // var gridOffsetPercentage = lineStrips[0].gridOffsetX / lineStrips[0].gridSize;
+            // var gridOffsetFactor = Math.cos(gridOffsetPercentage * Math.PI);
+            node.a0 = getDarkness(HeightMap.frame + 10);
+            node.a1 = 0;
             node.a2 = 0;
-            node.b1 = -0.72;
+            node.b1 = Math.map(Math.pow(HeightMap.getWaviness(), 2), 0, 1, -0.93, -0.3);
             node.b2 = 0;
         }
 
         var biquadFilter = (function() {
-            var bufferSize = 4096;
-            var node = audioContext.createScriptProcessor(undefined, 1, 1);
+            var node = audioContext.createScriptProcessor(1024, 1, 1);
             node.a0 = 1;
             node.a1 = 0.0;
             node.a2 = 0;
             node.b1 = 0;
             node.b2 = 0;
             node.onaudioprocess = function(e) {
+                // console.log(getDarkness(HeightMap.frame));
                 var input = e.inputBuffer.getChannelData(0);
                 var output = e.outputBuffer.getChannelData(0);
-                for (var n = 0; n < bufferSize; n++) {
+                for (var n = 0; n < e.inputBuffer.length; n++) {
                     var time = e.playbackTime + n / audioContext.sampleRate;
                     setBiquadParameters(node, time);
                     var x = input[n];
@@ -178,9 +182,13 @@
             }
             return node;
         })();
-        noiseGain.connect(biquadFilter);
+        noise.connect(biquadFilter);
 
-        biquadFilter.connect(audioContext.gain);
+        var biquadFilterGain = audioContext.createGain();
+        biquadFilterGain.gain.value = 0.05;
+        biquadFilter.connect(biquadFilterGain);
+
+        biquadFilterGain.connect(audioContext.gain);
         return {
             biquadFilter: biquadFilter
         };
@@ -203,11 +211,11 @@
         renderer.autoClearColor = false;
         renderer.setClearColor(0xfcfcfc, 1);
         renderer.clear();
-        canvas = renderer.domElement;
-        canvasWidth = canvas.width;
-        canvasHeight = canvas.height;
-        mouseX = canvasWidth;
-        mouseY = canvasHeight;
+        var canvas = renderer.domElement;
+        HeightMap.canvasWidth = canvas.width;
+        HeightMap.canvasHeight = canvas.height;
+        mouseX = canvas.width;
+        mouseY = canvas.height;
         scene = new THREE.Scene();
         camera = new THREE.OrthographicCamera(0, canvas.width, 0, canvas.height, 1, 1000);
         camera.position.z = 500;
@@ -226,24 +234,33 @@
         var opacityChangeFactor = 0.1;
         if (isMouseDown) {
             lineMaterial.opacity = lineMaterial.opacity * (1 - opacityChangeFactor) + 0.23 * opacityChangeFactor;
-            frame += 4;
+            HeightMap.frame += 4;
         } else {
             lineMaterial.opacity = lineMaterial.opacity * (1 - opacityChangeFactor) + 0.03 * opacityChangeFactor;
-            frame += 1;
+            HeightMap.frame += 1;
         }
 
-        if (frame % 1000 < 500) {
+        if (HeightMap.frame % 1000 < 500) {
             lineMaterial.color.set("rgb(50, 12, 12)");
         } else {
             lineMaterial.color.set("rgb(252, 247, 243)");
         }
 
-        var dx = Math.map(mouseX, 0, canvasWidth, -1, 1) * 2.20;
-        var dy = Math.map(mouseY, 0, canvasHeight, -1, 1) * 2.20;
+        var dx = Math.map(mouseX, 0, HeightMap.canvasWidth, -1, 1) * 2.20;
+        var dy = Math.map(mouseY, 0, HeightMap.canvasHeight, -1, 1) * 2.20;
         lineStrips.forEach(function (lineStrip) {
             lineStrip.update(dx, dy);
         });
         renderer.render(scene, camera);
+    }
+
+    // return a number from [0..1] indicating in general how dark the image is; 1.0 means very dark, while 0.0 means very light
+    function getDarkness(frame) {
+        if (frame % 1000 < 500) {
+            return Math.map(frame % 500, 0, 500, 0, 1);
+        } else {
+            return Math.map(frame % 500, 0, 500, 1, 0);
+        }
     }
 
     function mousemove(event) {
@@ -261,8 +278,8 @@
     }
 
     function resize(width, height) {
-        canvasWidth = width;
-        canvasHeight = height;
+        HeightMap.canvasWidth = width;
+        HeightMap.canvasHeight = height;
         camera.right = width;
         camera.bottom = height;
         camera.updateProjectionMatrix();
@@ -271,7 +288,7 @@
         renderer.clear();
 
         // draw black again
-        frame = 0;
+        HeightMap.frame = 0;
 
         lineStrips.forEach(function (lineStrip) {
             lineStrip.resize(width, height);
