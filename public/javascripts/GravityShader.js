7aeedbbb2f2634dd4799b48c1ad7ad3aa3d41a41
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-03-13 02:30:22 -0400

    Reflected particle location somewhat controlled by mouse

--- public/javascripts/GravityShader.js
+++ public/javascripts/GravityShader.js
@@ -2,6 +2,7 @@ GravityShader = {
     uniforms: {
         iGlobalTime: { type: 'f', value: 0 },
         iMouse:      { type: 'v2', value: new THREE.Vector2(0, 0) },
+        iMouseFactor:{ type: 'f', value: 1/15 },
         iResolution: { type: 'v2', value: new THREE.Vector2(100, 100) },
         G:           { type: 'f', value: 0 },
         tDiffuse:    { type: 't', value: null }
@@ -18,19 +19,20 @@ GravityShader = {
     fragmentShader: [
         "uniform float iGlobalTime;",
         "uniform vec2 iMouse;",
+        "uniform float iMouseFactor;",
         "uniform vec2 iResolution;",
         "uniform sampler2D tDiffuse;",
         "varying vec2 vTextureCoord;",
         "uniform float G;",
 
-        "vec2 gravity(vec2 p, vec2 m, float g) {",
+        "vec2 gravity(vec2 p, vec2 attractionCenter, float g) {",
         "    float mass1 = 1.0;",
         "    float mass2 = 1.0;",
         "    ",
-        "    return (m - p) * g * mass1 * mass2 / pow(length(p - m), 2.0);",
+        "    return (attractionCenter - p) * g * mass1 * mass2 / pow(length(p - attractionCenter), 2.0);",
         "}",
 
-        "vec4 equality(vec2 p, vec2 m) {",
+        "vec4 equality(vec2 p, vec2 attractionCenter) {",
         "    float total = 0.0;",
         "    vec2 incomingP = p;",
         "    vec2 outgoingP = p;",
@@ -38,10 +40,12 @@ GravityShader = {
         "    vec4 outgoingColorFactor = vec4(0.93, 1.0, 1.0 / 0.93, 1.0);",
         "    vec4 incomingColorFactor = vec4(1.0 / 0.88, 1.0, 0.88, 1.0);",
         "    for( float i = 1.0; i < 12.0; i += 1.0) {",
-        "       incomingP = incomingP - gravity(incomingP, m, G * 2.0);",
-        "       outgoingP = outgoingP + gravity(outgoingP, m, G * 20.0);",
-        "       c += texture2D(tDiffuse, incomingP / iResolution) / (i*i + 2.8) * pow(incomingColorFactor, vec4(i));",
-        "       c += texture2D(tDiffuse, outgoingP / iResolution) / (i*i + 2.8) * pow(outgoingColorFactor, vec4(i));",
+        "       incomingP = incomingP - gravity(incomingP, attractionCenter, G * 1.0);",
+        "       outgoingP = outgoingP + gravity(outgoingP, attractionCenter, G * 10.0);",
+        "       incomingP -= (iMouse - p) * iMouseFactor;",
+        "       outgoingP += (iMouse - p) * iMouseFactor;",
+        "       c += texture2D(tDiffuse, incomingP / iResolution) / (i*i + 3.5) * pow(incomingColorFactor, vec4(i));",
+        "       c += texture2D(tDiffuse, outgoingP / iResolution) / (i*i + 3.5) * pow(outgoingColorFactor, vec4(i));",
         "    }",
         "    return c;",
         "}",
@@ -49,10 +53,9 @@ GravityShader = {
         "void main(void)",
         "{",
         "    vec2 uv = gl_FragCoord.xy;",
-        "    vec2 uvM = iMouse.xy;",
         "    vec4 c = texture2D(tDiffuse, vTextureCoord);",
-        "    vec4 c2 = equality(uv, uvM);",
-        "    gl_FragColor = pow(c + c2, vec4(3.5/6.0));",
+        "    vec4 c2 = equality(uv, iResolution/2.0);",
+        "    gl_FragColor = pow(c + c2, vec4(3.2/6.0));",
         "}"
     ].join("\n")
 }
