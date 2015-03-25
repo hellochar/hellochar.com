ddd21be7fd56d0000b79167c29cafc9c372cb4ac
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-03-24 23:28:49 -0700

    starting world gen sketch; basic conversion of iq's landscape algo

--- public/javascripts/math.js
+++ public/javascripts/math.js
@@ -5,4 +5,11 @@
     Math.map = function (x, xStart, xStop, yStart, yStop) {
         return Math.lerp(yStart, yStop, (x - xStart) / (xStop - xStart));
     };
+
 })(Math);
+
+THREE.Vector2.prototype.multiply = function(v) {
+    this.x *= v.x;
+    this.y *= v.y;
+    return this;
+}
