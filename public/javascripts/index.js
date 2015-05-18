b46e00777f14259645323fafea375be21bdbb99c
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-05-17 19:55:27 -0700

    link to single sketches on main page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,6 +6,8 @@
         var sketch = getSketch(sketchName);
         var $sketchElement = initializeSketch(sketch, $allSketches);
 
+        $sketchElement.prepend("<a class='new-window' target='_blank' href='" + sketchName + "'><i class='fa fa-external-link'></i></a>");
+
         // add sketch element to nav
         var $navElement = $('<li></li>');
         $navElement.text(sketch.id)
