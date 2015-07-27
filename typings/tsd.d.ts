cc5911ecc6a4091fb0c1fcbea5d8cf74ac4d1e24
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-07-27 02:28:11 -0700

    initial move to typescript for the game

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="threejs/three.d.ts" />
+/// <reference path="webaudioapi/waa.d.ts" />
+/// <reference path="webrtc/MediaStream.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
