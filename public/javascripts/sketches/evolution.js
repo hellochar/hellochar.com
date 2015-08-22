00903b5cf7a9b91bc96a485ee6d06053603ccfa0
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-08-21 19:19:50 -0700

    plants and animals reproduce

--- public/javascripts/sketches/evolution.js
+++ public/javascripts/sketches/evolution.js
@@ -5,6 +5,16 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var Evolution;
 (function (Evolution) {
+    function clampHue(hue) {
+        return ((hue % 1) + 1) % 1;
+    }
+    function hueDistance(hue1, hue2) {
+        var angle1 = hue1 * Math.PI * 2, angle2 = hue2 * Math.PI * 2;
+        var vector1 = new THREE.Vector2(Math.cos(angle1), Math.sin(angle1)), vector2 = new THREE.Vector2(Math.cos(angle2), Math.sin(angle2));
+        var dot = vector1.dot(vector2);
+        var angle = Math.acos(dot);
+        var dist = angle / (Math.PI * 2);
+    }
     var Thing = (function () {
         function Thing(world, position) {
             this.world = world;
@@ -21,15 +31,25 @@ var Evolution;
         function Plant(world, hue, position) {
             _super.call(this, world, position);
             this.hue = hue;
-            var geometry = new THREE.CircleGeometry(1);
+            this.time = 0;
+            var geometry = new THREE.CircleGeometry(0.5);
             var color = (new THREE.Color()).setHSL(this.hue, 1, 0.5);
             var material = new THREE.MeshBasicMaterial({
                 color: color.getHex()
             });
             this.mesh = new THREE.Mesh(geometry, material);
             this.updateMesh();
+            this.time = 800 + Math.random() * 500;
         }
         Plant.prototype.run = function () {
+            this.time -= 1;
+            if (this.time <= 0) {
+                var position = new THREE.Vector2(this.position.x + 15 * Math.random() - 7.5, this.position.y + 15 * Math.random() - 7.5);
+                var hue = clampHue(this.hue + 2 * (Math.random() - 0.5) * 0.05);
+                var plant = new Plant(this.world, hue, position);
+                this.world.add(plant);
+                this.time = 800 + Math.random() * 500;
+            }
         };
         return Plant;
     })(Thing);
@@ -40,6 +60,7 @@ var Evolution;
             _super.call(this, world, position);
             this.hue = hue;
             this.health = 1000;
+            this.time = 0;
             var geometry = new THREE.PlaneGeometry(3, 3);
             var color = (new THREE.Color()).setHSL(this.hue, 1, 0.5);
             var material = new THREE.MeshBasicMaterial({
@@ -47,29 +68,39 @@ var Evolution;
             });
             this.mesh = new THREE.Mesh(geometry, material);
             this.updateMesh();
+            this.time = 2500 + Math.random() * 2500;
         }
         Animal.prototype.run = function () {
             var _this = this;
             this.health -= 1;
+            this.time -= 1;
+            if (this.time <= 0) {
+                var position = new THREE.Vector2(this.position.x + 15 * Math.random() - 7.5, this.position.y + 15 * Math.random() - 7.5);
+                var animal = new Animal(this.world, this.hue, position);
+                this.world.add(animal);
+                this.time = 2500 + Math.random() * 2500;
+            }
             if (this.health < 500) {
-                var bestFood = this.world.plants.reduce(function (bestPlant, currentPlant) {
-                    var distToBest = _this.position.distanceTo(bestPlant.position);
-                    var distToCurr = _this.position.distanceTo(currentPlant.position);
-                    if (distToBest < distToCurr) {
-                        return bestPlant;
+                if (this.world.plants.length > 0) {
+                    var bestFood = this.world.plants.reduce(function (bestPlant, currentPlant) {
+                        var distToBest = _this.position.distanceTo(bestPlant.position);
+                        var distToCurr = _this.position.distanceTo(currentPlant.position);
+                        if (distToBest < distToCurr) {
+                            return bestPlant;
+                        }
+                        else {
+                            return currentPlant;
+                        }
+                    });
+                    var offset = bestFood.position.clone().sub(this.position);
+                    if (offset.length() < 1) {
+                        this.health += 500;
+                        this.world.destroy(bestFood);
                     }
-                    else {
-                        return currentPlant;
-                    }
-                });
-                var offset = bestFood.position.clone().sub(this.position);
-                if (offset.length() < 1) {
-                    this.health += 500;
-                    this.world.destroy(bestFood);
+                    var speed = 0.7;
+                    offset.setLength(speed);
+                    this.position.add(offset);
                 }
-                var speed = 0.7;
-                offset.setLength(speed);
-                this.position.add(offset);
             }
             if (this.health < 0) {
                 this.world.destroy(this);
@@ -92,16 +123,24 @@ var Evolution;
             for (var i = 0; i < 25; i++) {
                 var position = new THREE.Vector2(Math.random() * extent - extent / 2, Math.random() * extent - extent / 2);
                 var plant = new Plant(this, Math.random(), position);
-                this.plants.push(plant);
-                scene.add(plant.mesh);
+                this.add(plant);
             }
             for (var i = 0; i < 5; i++) {
                 var position = new THREE.Vector2(Math.random() * extent - extent / 2, Math.random() * extent - extent / 2);
                 var animal = new Animal(this, Math.random(), position);
-                this.animals.push(animal);
-                scene.add(animal.mesh);
+                this.add(animal);
             }
         }
+        World.prototype.add = function (thing) {
+            if (thing instanceof Animal) {
+                this.animals.push(thing);
+                this.scene.add(thing.mesh);
+            }
+            if (thing instanceof Plant) {
+                this.plants.push(thing);
+                this.scene.add(thing.mesh);
+            }
+        };
         World.prototype.destroy = function (thing) {
             if (thing instanceof Animal) {
                 this.animals.splice(this.animals.indexOf(thing), 1);
@@ -114,6 +153,7 @@ var Evolution;
         };
         World.prototype.run = function () {
             this.plants.forEach(function (p) {
+                p.run();
                 p.updateMesh();
             });
             this.animals.forEach(function (a) {
