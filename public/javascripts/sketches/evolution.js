e39252bb6733c7754d62ef64a5ec194f465aface
Xiaohan Zhang <hellocharlien@hotmail.com>
2015-10-18 16:34:25 -0700

    pretty interesting growth behaviors

--- public/javascripts/sketches/evolution.js
+++ public/javascripts/sketches/evolution.js
@@ -34,34 +34,47 @@ var Evolution;
             this.hue = hue;
             this.health = health;
             this.time = 0;
-            var geometry = new THREE.CircleGeometry(0.5);
+            this.nutrientsConsumed = 0;
             var color = (new THREE.Color()).setHSL(this.hue, 1, 0.5);
             var material = new THREE.MeshBasicMaterial({
                 color: color.getHex()
             });
-            this.mesh = new THREE.Mesh(geometry, material);
+            this.mesh = new THREE.Mesh(Plant.GEOMETRY, material);
             this.updateMesh();
-            this.time = 800 + Math.random() * 500;
+            this.resetTime();
         }
-        Plant.prototype.run = function () {
-            this.time -= 1;
-            this.health -= 1;
-            this.health += this.world.requestNutrients(this.position, 1);
-            this.health += this.world.requestNutrients(this.position, 0.25, 0, 1);
-            this.health += this.world.requestNutrients(this.position, 0.25, 1, 0);
-            this.health += this.world.requestNutrients(this.position, 0.25, 0, -1);
-            this.health += this.world.requestNutrients(this.position, 0.25, -1, 0);
-            if (this.time <= 0) {
-                var position = new THREE.Vector2(this.position.x + 15 * Math.random() - 7.5, this.position.y + 15 * Math.random() - 7.5);
-                var hue = clampHue(this.hue + 2 * (Math.random() - 0.5) * 0.05);
-                var newPlantHealth = this.health / 10;
-                var plant = new Plant(this.world, hue, position, newPlantHealth);
-                this.health -= newPlantHealth;
-                this.world.add(plant);
-                this.time = 800 + Math.random() * 500;
+        Plant.prototype.resetTime = function () {
+            this.time = 800 + Math.random() * 500;
+        };
+        Plant.prototype.run = function (delta) {
+            this.time -= delta;
+            this.health -= delta;
+            var oldHealth = this.health;
+            this.health += this.world.requestNutrients(this.position, delta * 1);
+            this.health += this.world.requestNutrients(this.position, delta * 0.2, 0, 1);
+            this.health += this.world.requestNutrients(this.position, delta * 0.2, 1, 0);
+            this.health += this.world.requestNutrients(this.position, delta * 0.2, 0, -1);
+            this.health += this.world.requestNutrients(this.position, delta * 0.2, -1, 0);
+            this.nutrientsConsumed += (this.health - oldHealth);
+            if (this.time <= 0 && this.health > 500) {
+                for (var z = 0; z < 1; z++) {
+                    var move = 3;
+                    var position = new THREE.Vector2(this.position.x + move * (Math.random() - 0.5), this.position.y + move * (Math.random() - 0.5));
+                    var hue = clampHue(this.hue + 2 * (Math.random() - 0.5) * 0.05);
+                    var newPlantHealth = this.health / 10;
+                    var plant = new Plant(this.world, hue, position, newPlantHealth);
+                    this.health -= newPlantHealth;
+                    this.world.add(plant);
+                }
+                this.resetTime();
             }
             if (this.health < 0) {
                 this.world.destroy(this);
+                this.world.requestNutrients(this.position, -this.nutrientsConsumed / 2);
+                this.world.requestNutrients(this.position, -this.nutrientsConsumed / 8, 0, 1);
+                this.world.requestNutrients(this.position, -this.nutrientsConsumed / 8, 1, 0);
+                this.world.requestNutrients(this.position, -this.nutrientsConsumed / 8, 0, -1);
+                this.world.requestNutrients(this.position, -this.nutrientsConsumed / 8, -1, 0);
             }
         };
         Plant.prototype.updateMesh = function () {
@@ -69,15 +82,17 @@ var Evolution;
             var scale = this.health / 1000;
             this.mesh.scale.set(scale, scale, 1);
         };
+        Plant.GEOMETRY = new THREE.CircleGeometry(0.5);
         return Plant;
     })(Thing);
     Evolution.Plant = Plant;
     var Animal = (function (_super) {
         __extends(Animal, _super);
-        function Animal(world, hue, position) {
+        function Animal(world, hue, position, health) {
+            if (health === void 0) { health = 2000; }
             _super.call(this, world, position);
             this.hue = hue;
-            this.health = 1000;
+            this.health = health;
             this.time = 0;
             var geometry = new THREE.PlaneGeometry(3, 3);
             var color = (new THREE.Color()).setHSL(this.hue, 1, 0.5);
@@ -86,19 +101,22 @@ var Evolution;
             });
             this.mesh = new THREE.Mesh(geometry, material);
             this.updateMesh();
-            this.time = 2500 + Math.random() * 2500;
+            this.time = 5000 + Math.random() * 2500;
         }
-        Animal.prototype.run = function () {
+        Animal.prototype.run = function (delta) {
             var _this = this;
-            this.health -= 1;
-            this.time -= 1;
-            if (this.time <= 0) {
-                var position = new THREE.Vector2(this.position.x + 15 * Math.random() - 7.5, this.position.y + 15 * Math.random() - 7.5);
-                var animal = new Animal(this.world, this.hue, position);
+            this.health -= delta;
+            this.time -= delta;
+            if (this.time <= 0 && this.health > 500) {
+                var birthOffset = 3;
+                var position = new THREE.Vector2(this.position.x + birthOffset * (Math.random() - 0.5), this.position.y + birthOffset * (Math.random() - 0.5));
+                var childHealth = this.health / 2;
+                var animal = new Animal(this.world, this.hue, position, childHealth);
                 this.world.add(animal);
-                this.time = 2500 + Math.random() * 2500;
+                this.health -= childHealth;
+                this.time = 5000 + Math.random() * 2500;
             }
-            if (this.health < 500) {
+            if (this.health < 1000) {
                 if (this.world.plants.length > 0) {
                     var bestFood = this.world.plants.reduce(function (bestPlant, currentPlant) {
                         var distToBest = _this.position.distanceTo(bestPlant.position);
@@ -111,14 +129,20 @@ var Evolution;
                         }
                     });
                     var offset = bestFood.position.clone().sub(this.position);
-                    if (offset.length() < 1) {
-                        this.health += 500;
-                        this.world.destroy(bestFood);
+                    if (offset.length() < 1 * delta) {
+                        this.health += bestFood.health / 2;
+                        bestFood.health /= 2;
                     }
-                    var speed = 0.7;
-                    offset.setLength(speed);
-                    this.position.add(offset);
+                    else {
+                        offset.setLength(Animal.SPEED * delta);
+                        this.position.add(offset);
+                    }
                 }
+            }
+            else {
+                var angle = Math.random() * Math.PI * 2;
+                this.position.x += Math.cos(angle) * 0.1 * delta;
+                this.position.y += Math.sin(angle) * 0.1 * delta;
             }
             if (this.health < 0) {
                 this.world.destroy(this);
@@ -129,6 +153,7 @@ var Evolution;
             var scale = this.health / 1000;
             this.mesh.scale.set(scale, scale, 1);
         };
+        Animal.SPEED = 1;
         return Animal;
     })(Thing);
     Evolution.Animal = Animal;
@@ -139,12 +164,14 @@ var Evolution;
             this.extent = extent;
             this.plants = [];
             this.animals = [];
+            this.time = 0;
+            this.totalNutrients = 0;
             this.nutrients = new Array(extent * extent);
             for (var i = 0; i < extent * extent; i++) {
-                this.nutrients[i] = 1000;
+                this.nutrients[i] = 2000;
             }
-            for (var i = 0; i < 1; i++) {
-                var position = new THREE.Vector2(0, 0);
+            for (var i = 0; i < 10; i++) {
+                var position = new THREE.Vector2(Math.random() * extent - extent / 2, Math.random() * extent - extent / 2);
                 var plant = new Plant(this, Math.random(), position);
                 this.add(plant);
             }
@@ -178,7 +205,7 @@ var Evolution;
             }
             var index = gridY * this.extent + gridX;
             var actualAmount = Math.min(amount, this.nutrients[index]);
-            this.nutrients[index] -= actualAmount;
+            this.nutrients[index] = Math.min(this.nutrients[index] - actualAmount, 2000);
             return actualAmount;
         };
         World.prototype.add = function (thing) {
@@ -194,40 +221,42 @@ var Evolution;
         World.prototype.destroy = function (thing) {
             if (thing instanceof Animal) {
                 this.animals.splice(this.animals.indexOf(thing), 1);
-                this.scene.remove(thing.mesh);
             }
             if (thing instanceof Plant) {
                 this.plants.splice(this.plants.indexOf(thing), 1);
-                this.scene.remove(thing.mesh);
             }
+            this.scene.remove(thing.mesh);
         };
         World.prototype.run = function () {
             var _this = this;
-            var now = Date.now();
-            for (var i = 0; i < 100; i++) {
+            var delta = 450;
+            for (var i = 0; i < 1; i++) {
                 this.plants.forEach(function (p) {
-                    p.run();
+                    p.run(delta);
                     p.updateMesh();
                 });
                 this.animals.forEach(function (a) {
-                    a.run();
+                    a.run(delta);
                     a.updateMesh();
                 });
-                for (var j = 0; j < 10; j++) {
+                for (var j = 0; j < 10 * delta; j++) {
                     var index = Math.floor(Math.random() * this.nutrients.length);
-                    this.nutrients[index] = Math.min(this.nutrients[index] + 10, 2000);
+                    this.nutrients[index] = Math.min(this.nutrients[index] + 10 * this.extent / 100, 2000);
                 }
-                if (Date.now() - now > 0) {
-                    break;
-                }
+                this.time += delta;
             }
             var richColor = new THREE.Color("#15711E");
             var barrenColor = new THREE.Color("#906D22");
+            this.totalNutrients = 0;
             this.nutrientsObject.geometry.colors.forEach(function (color, index) {
                 var nutrients = _this.nutrients[index];
                 color.set("#906D22");
                 color.lerp(richColor, nutrients / 2000);
+                _this.totalNutrients += nutrients;
             });
+            if (this.plants.length > 0) {
+                console.log(this.time + ", " + this.totalNutrients);
+            }
             this.nutrientsObject.geometry.colorsNeedUpdate = true;
         };
         return World;
