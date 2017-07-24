5c5f2a12fbe955e5718b5285cbff7c6572b3c82a
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-07-24 01:01:56 -0700

    delete references to bower_components

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'docs')));
 
 // app.use('/', routes);
