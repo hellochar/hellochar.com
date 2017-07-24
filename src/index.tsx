de183e88544edba4aa3327e4e14d4f906ae5e491
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-07-23 23:15:57 -0700

    use react, new typescript, on docs page

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+
+ReactDOM.render(
+    <div>
+        Hello world!
+    </div>,
+    root);
