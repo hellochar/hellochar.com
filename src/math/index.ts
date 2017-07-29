ba336a0ac1b096cbab77ee7ffc4295434426df04
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-07-28 20:30:35 -0700

    add waves sketch

---
+++ src/math/index.ts
@@ -0,0 +1,7 @@
+export function lerp(a: number, b: number, x: number) {
+    return a + (b - a) * x;
+};
+
+export function map(x: number, xStart: number, xStop: number, yStart: number, yStop: number) {
+    return lerp(yStart, yStop, (x - xStart) / (xStop - xStart));
+};
