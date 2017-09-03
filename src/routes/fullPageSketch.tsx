7bf733eaee4ce424356869941dc4f371ae2a64eb
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-09-03 02:43:20 -0700

    just use a back button on sketch pages

--- src/routes/fullPageSketch.tsx
+++ src/routes/fullPageSketch.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
+
 import { SketchComponent } from "../sketchComponent";
 import { ISketch } from "../sketch";
 import { ShrinkingHeader } from "./shrinkingHeader";
@@ -12,11 +14,12 @@ export class FullPageSketch extends React.Component<ISketchRouteProps, {}> {
     public render() {
         return (
             <div className="full-page-sketch">
-                <ShrinkingHeader
+                <Link className="back-button" to="/">&#10094;</Link>
+                {/* <ShrinkingHeader
                     alwaysShrunken
                     darkTheme={this.props.sketch.darkTheme}
                     onlyShowOnHover
-                />
+                /> */}
                 <SketchComponent sketch={this.props.sketch} />
             </div>
         );
