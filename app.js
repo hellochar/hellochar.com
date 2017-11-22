e236d72d1b41825ddf9718ec88d442ad72ab46a7
Xiaohan Zhang <xzhang@palantir.com>
2017-11-21 17:19:02 -0800

    fix server; add drawing with gravitational waves

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-
 var app = express();
 
 // uncomment after placing your favicon in /public
