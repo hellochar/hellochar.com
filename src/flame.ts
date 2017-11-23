a24f852a7ef2265e71ec4e7c76e62adcf4cbc9b5
Xiaohan Zhang <xzhang@palantir.com>
2017-11-23 04:44:25 -0700

    randomly generate new flames
    press space to randomly generate one
    
    good naming
    more affines
    more variations
    interpolated and router variations

--- src/flame.ts
+++ src/flame.ts
@@ -2,39 +2,76 @@ import * as THREE from 'three';
 
 import { map } from './math';
 import { ISketch, SketchAudioContext } from './sketch';
-import { MouseEvent } from 'react';
+import { MouseEvent, KeyboardEvent } from 'react';
 
-type Variation = (point: THREE.Vector3) => void;
+type Transform = (point: THREE.Vector3) => void;
 
-interface Affine {
+interface Branch {
     color: THREE.Color;
-    weight: number;
-    transform: Variation;
-    variation: Variation;
+    affine: Transform;
+    variation: Transform;
+}
+
+const AFFINES = {
+    // lerp halfway towards the origin, biased by -1
+    TowardsOriginNegativeBias: (point: THREE.Vector3) => {
+        point.set( (point.x - 1) / 2 + 0.25, (point.y - 1) / 2, point.z / 2);
+    },
+    // lerp towards the origin, biasing x by 1, y by -1
+    TowardsOrigin2: (point: THREE.Vector3) => {
+        point.set( (point.x + 1) / 2, (point.y - 1) / 2, (point.z + 1) / 2);
+    },
+    Swap: (point: THREE.Vector3) => {
+        point.set((point.y + point.z) / 2.5, (point.x + point.z) / 2.5, (point.x + point.y) / 2.5);
+    },
+    SwapSub: (point: THREE.Vector3) => {
+        point.set((point.y - point.z) / 2, (point.z - point.x) / 2, (point.x - point.y) / 2);
+    },
+    Negate: (point: THREE.Vector3) => {
+        point.set(-point.x, -point.y, -point.z);
+    },
+    NegateSwap: (point: THREE.Vector3) => {
+        point.set(
+            (-point.x + point.y + point.z) / 2.1,
+            (-point.y + point.x + point.z) / 2.1,
+            (-point.z + point.x + point.y) / 2.1
+        );
+    },
+    Up1: (point: THREE.Vector3) => {
+        point.set(point.x, point.y, point.z + 1);
+    }
 }
 
 const VARIATIONS = {
     Linear: (point: THREE.Vector3) => {
         // no op
     },
-    Sinusoidal: (point: THREE.Vector3) => {
-        point.set(Math.sin(point.x), Math.sin(point.y), point.z);
+    Sin: (point: THREE.Vector3) => {
+        point.set(Math.sin(point.x), Math.sin(point.y), Math.sin(point.z));
     },
     Spherical: (point: THREE.Vector3) => {
         point.multiplyScalar(1 / point.lengthSq());
     },
     Polar: (point: THREE.Vector3) => {
-        point.set(Math.atan2(point.y, point.x) / Math.PI, point.length() - 1, point.z);
+        point.set(Math.atan2(point.y, point.x) / Math.PI, point.length() - 1, Math.atan2(point.z, point.x));
     },
     Swirl: (point: THREE.Vector3) => {
         var r2 = point.lengthSq();
-        point.set(point.x * Math.sin(r2) - point.y * Math.cos(r2),
-                    point.x * Math.cos(r2) + point.y * Math.sin(r2),
-                    point.z);
+        point.set(point.z * Math.sin(r2) - point.y * Math.cos(r2),
+                    point.x * Math.cos(r2) + point.z * Math.sin(r2),
+                    point.x * Math.sin(r2) - point.y * Math.sin(r2),
+                );
     },
+    Normalize: (point: THREE.Vector3) => {
+        // point.setLength(Math.sqrt(point.length()));
+        point.normalize();
+    },
+    Shrink: (point: THREE.Vector3) => {
+        point.setLength(Math.exp(-point.lengthSq()));
+    }
 };
 
-function createInterpolatedVariation(variationA: Variation, variationB: Variation, interpolationFn: () => number) {
+function createInterpolatedVariation(variationA: Transform, variationB: Transform, interpolationFn: () => number) {
     return function(pointA: THREE.Vector3) {
         var pointB = pointA.clone();
         variationA(pointA);
@@ -44,102 +81,154 @@ function createInterpolatedVariation(variationA: Variation, variationB: Variatio
     };
 }
 
-function stepAffine(affine: Affine, point: THREE.Vector3, color: THREE.Color) {
+function createRouterVariation(vA: Transform, vB: Transform, router: (p: THREE.Vector3) => boolean) {
+    return (a: THREE.Vector3) => {
+        const choice = router(a);
+        if (choice) {
+            vA(a);
+        } else {
+            vB(a);
+        }
+    }
+}
+
+/**
+ * @param branch branch to apply
+ * @param point point to apply to
+ * @param color color to apply to
+ */
+function applyBranch(branch: Branch, point: THREE.Vector3, color: THREE.Color) {
     // apply the affine transform to the point
-    affine.transform(point);
+    branch.affine(point);
+    point.x += cX / 5;
+    point.y += cY / 5;
 
     // apply the nonlinear variation to the point
-    affine.variation(point);
+    branch.variation(point);
 
     // interpolate towards the affine color
     // color.lerp(affine.color, 0.5);
-    color.add(affine.color);
+    color.add(branch.color);
 }
 
-class AffineSet {
-    public totalWeight: number;
-    constructor (public affines: Affine[]) {
-        this.totalWeight = this.affines.map(function (affine) { return affine.weight; }).reduce(function(x,y) { return x+y; });
-    }
-
-    public choose() {
-        const weight = Math.random() * this.totalWeight;
-        let chosenAffine = this.affines[0];
-        this.affines.reduce(function (oldWeightSum, thisAffine) {
-            if (oldWeightSum < weight && weight <= thisAffine.weight + oldWeightSum + 1e-10) {
-                chosenAffine = thisAffine;
-            };
-            return thisAffine.weight + oldWeightSum;
-        }, 0);
-        return chosenAffine;
-    }
-
-    public step(point: THREE.Vector3, color: THREE.Color) {
-        const affine = this.choose();
-        stepAffine(affine, point, color);
-    }
-}
-
-const CIRCLE_CLAWS = new AffineSet([
+const CIRCLE_CLAWS: Branch[] = [
     {
         color: new THREE.Color(0.28, -0.12, -0.12),
-        weight: 1,
-        transform: function(point) {
-            point.set((cX + point.x) / 2, (cY+point.y) / 2, point.z);
-        },
+        affine: AFFINES.TowardsOriginNegativeBias,
         variation: VARIATIONS.Swirl
     },
     {
         color: new THREE.Color(0.07, 0.21, 0),
-        weight: 1,
-        transform: function(point) {
-            point.set( (-1 + point.x) / 2 + 0.25, (-1 + point.y) / 2, point.z);
-        },
+        affine: AFFINES.TowardsOriginNegativeBias,
         variation: VARIATIONS.Spherical
     },
     {
         color: new THREE.Color(0, 0.1, 0.35),
-        weight: 1,
-        transform: function(point) {
-            point.set( (1 + point.x) / 2, (-1 + point.y) / 2, point.z);
-        },
+        affine: AFFINES.TowardsOrigin2,
         variation: VARIATIONS.Polar
     },
-]);
+];
+
+let branches: Branch[] = CIRCLE_CLAWS;
 
 class SuperPoint {
     public children: SuperPoint[];
     constructor(
         public point: THREE.Vector3,
         public color: THREE.Color,
-        public rootGeometry: THREE.Geometry) {
-        this.point = point;
-        this.color = color;
-        this.rootGeometry = rootGeometry;
+        public rootGeometry: THREE.Geometry,
+        private branches: Branch[]
+    ) {
         rootGeometry.vertices.push(point);
         rootGeometry.colors.push(color);
     }
 
-    public updateSubtree(affineSet: AffineSet, depth: number) {
+    public updateSubtree(depth: number) {
         if (depth === 0) return;
 
         if (this.children === undefined) {
-            this.children = affineSet.affines.map(() => {
-                return new SuperPoint(new THREE.Vector3(), new THREE.Color(0, 0, 0), this.rootGeometry);
+            this.children = branches.map(() => {
+                return new SuperPoint(
+                    new THREE.Vector3(),
+                    new THREE.Color(0, 0, 0),
+                    this.rootGeometry,
+                    this.branches,
+                );
             });
         }
 
         this.children.forEach((child, idx) => {
-            var affine = affineSet.affines[idx];
+            const branch = branches[idx];
             // reset the child's position to your updated position so it's ready to get stepped
             child.point.copy(this.point);
             child.color.copy(this.color);
-            stepAffine(affine, child.point, child.color);
-            child.updateSubtree(affineSet, depth - 1);
+            applyBranch(branch, child.point, child.color);
+            child.updateSubtree(depth - 1);
         });
+    }
+
+    public recalculate() {
+        const t = performance.now() / 10000;
+        const z = 1 / (1 + Math.exp(-10 * Math.sin(t)));;
+        this.point.set(0, 0, z / 100);
+        // this.point.set(0,0,0);
+        // we can support around 100k points. numPoints = branchLength ^ depth
+        // log(numPoints) = depth * log(branchLength)
+        // log(numPoints) / log(branchLength) = depth
+        const depth = Math.floor(Math.log(120000) / Math.log(branches.length));
+        // console.log(branches);
+        this.updateSubtree(depth);
     }
 }
 
+function randomBranches() {
+    const numAffines = Math.floor(map(Math.random(), 0, 1, 2, 7));
+    const branches: Branch[] = [];
+    for (let i = 0; i < numAffines; i++) {
+        branches.push(randomBranch(i));
+    }
+    return branches;
+}
+
+function randomBranch(idx: number) {
+    const affine = randomValue(AFFINES);
+    let variation = randomValue(VARIATIONS);
+
+    if (Math.random() < 0.2) {
+        variation = createInterpolatedVariation(
+            randomValue(VARIATIONS),
+            randomValue(VARIATIONS),
+            () => 0.5
+        );
+    } else if (Math.random() < 0.2) {
+        variation = createRouterVariation(
+            randomValue(VARIATIONS),
+            randomValue(VARIATIONS),
+            (p) => p.z < 0
+        );
+    }
+    const colorValues = [
+        Math.random() * 0.1 - 0.05,
+        Math.random() * 0.1 - 0.05,
+        Math.random() * 0.1 - 0.05
+    ];
+    const focusIndex = idx % 3;
+    colorValues[focusIndex] += 0.2;
+    const color = new THREE.Color().fromArray(colorValues);
+    console.log(color);
+    const branch: Branch = {
+        affine,
+        color,
+        variation,
+    }
+    return branch;
+}
+
+function randomValue<T>(obj: Record<string, T>) {
+    const keys = Object.keys(obj);
+    const index = Math.floor(Math.random() * keys.length);
+    return obj[keys[index]];
+}
 
 let renderer: THREE.WebGLRenderer;
 let camera: THREE.PerspectiveCamera;
@@ -168,14 +257,20 @@ function init(_renderer: THREE.WebGLRenderer, _audioContext: SketchAudioContext)
     camera.lookAt(new THREE.Vector3());
     controls = new THREE.OrbitControls(camera);
     controls.autoRotate = true;
+    controls.autoRotateSpeed = 0.5;
 
     geometry = new THREE.Geometry();
 
-    superPoint = new SuperPoint(new THREE.Vector3(0, 0, 0), new THREE.Color(0, 0, 0), geometry);
+    superPoint = new SuperPoint(
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Color(0, 0, 0),
+        geometry,
+        branches,
+    );
 
-    var material = new THREE.PointCloudMaterial({
+    material = new THREE.PointCloudMaterial({
         vertexColors: THREE.VertexColors,
-        size: 0.01,
+        size: 0.003,
         transparent: true,
         opacity: 0.7,
         sizeAttenuation: true
@@ -184,28 +279,10 @@ function init(_renderer: THREE.WebGLRenderer, _audioContext: SketchAudioContext)
     pointCloud = new THREE.PointCloud(geometry, material);
     pointCloud.rotateX(-Math.PI / 2);
     scene.add(pointCloud);
-
-    const light = new THREE.SpotLight(0xffffff, 1);
-    light.position.set(5, 20, 5);
-
-    scene.add(light);
-
-    const floor = new THREE.Mesh(
-        new THREE.PlaneGeometry(200, 200, 1, 1),
-        new THREE.MeshLambertMaterial({
-            color: 0xffffff,
-            opacity: 0.5,
-            transparent: true,
-        }),
-    );
-    floor.rotateX(-Math.PI / 2);
-    scene.add(floor);
 }
 
 function animate() {
-    const x = performance.now() / 3000;
-    superPoint.point.set(0,0,(Math.sin(x) + 1) / 25);
-    superPoint.updateSubtree(CIRCLE_CLAWS, 10);
+    superPoint.recalculate();
     geometry.verticesNeedUpdate = true;
 
     controls.update();
@@ -217,11 +294,35 @@ function mousemove(event: JQuery.Event) {
     var mouseX = event.offsetX == undefined ? (event.originalEvent as any).layerX : event.offsetX;
     var mouseY = event.offsetY == undefined ? (event.originalEvent as any).layerY : event.offsetY;
 
-    cX = Math.pow(map(mouseX, 0, renderer.domElement.width, -1, 4), 3);
-    cY = Math.pow(map(mouseY, 0, renderer.domElement.height, 4, -1), 3);
+    cX = Math.pow(map(mouseX, 0, renderer.domElement.width, -1.5, 1.5), 3);
+    cY = Math.pow(map(mouseY, 0, renderer.domElement.height, 1.5, -1.5), 3);
+    // cX = Math.pow(map(mouseX, 0, renderer.domElement.width, -0.5, 0.5), 1);
+    // cY = Math.pow(map(mouseY, 0, renderer.domElement.height, 0.5, -0.5), 1);
 }
 
 function mousedown(event: JQuery.Event) {
+}
+
+function keypress(event: JQuery.Event) {
+    if ((event.originalEvent as any).code === "Space") {
+        branches = randomBranches();
+
+        geometry = new THREE.Geometry();
+        geometry.vertices = [];
+        geometry.colors = [];
+        superPoint = new SuperPoint(
+            new THREE.Vector3(0, 0, 0),
+            new THREE.Color(0, 0, 0),
+            geometry,
+            branches,
+        );
+
+        scene.remove(pointCloud);
+
+        pointCloud = new THREE.PointCloud(geometry, material);
+        pointCloud.rotateX(-Math.PI / 2);
+        scene.add(pointCloud);
+    }
 }
 
 function resize() {
@@ -235,5 +336,6 @@ export const Flame: ISketch = {
     animate: animate,
     mousemove: mousemove,
     mousedown: mousedown,
+    keypress: keypress,
     resize: resize
 };
