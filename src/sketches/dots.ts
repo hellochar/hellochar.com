d052cd3ee2a7c94be59c0b1f606644f090eb1bfa
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-11-29 15:42:48 -0800

    noise for flame
    white noise
    bass synth + filter
    high synth
    major/minor chord
    compressor
    
    controlling points: point length variance
    
    volume modulated by velocity of particles, camera distance

--- src/sketches/dots.ts
+++ src/sketches/dots.ts
@@ -2,6 +2,7 @@ import * as $ from "jquery";
 import { parse } from "query-string";
 import * as THREE from "three";
 
+import { createWhiteNoise } from "../audio/noise";
 import { ExplodeShader } from "../common/explodeShader";
 import { ISketch, SketchAudioContext } from "../sketch";
 
@@ -23,45 +24,10 @@ const GRID_SIZE = parse(location.search).gridSize || 7;
 function createAudioGroup(audioContext: SketchAudioContext) {
 
     // white noise
-    const noise = (function() {
-        const node = audioContext.createBufferSource()
-        , buffer = audioContext.createBuffer(1, audioContext.sampleRate * 5, audioContext.sampleRate)
-        , data = buffer.getChannelData(0);
-        for (let i = 0; i < buffer.length; i++) {
-            data[i] = Math.random();
-        }
-        node.buffer = buffer;
-        node.loop = true;
-        node.start(0);
-        return node;
-    })();
+    const noise = createWhiteNoise(audioContext);
     const noiseGain = audioContext.createGain();
     noiseGain.gain.value = 0;
     noise.connect(noiseGain);
-
-    // // pink noise from http://noisehack.com/generate-noise-web-audio-api/
-    // var source = (function() {
-    //     var bufferSize = 4096;
-    //     var b0, b1, b2, b3, b4, b5, b6;
-    //     b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
-    //     var node = audioContext.createScriptProcessor(bufferSize, 1, 1);
-    //     node.onaudioprocess = function(e) {
-    //         var output = e.outputBuffer.getChannelData(0);
-    //         for (var i = 0; i < bufferSize; i++) {
-    //             var white = Math.random() * 2 - 1;
-    //             b0 = 0.99886 * b0 + white * 0.0555179;
-    //             b1 = 0.99332 * b1 + white * 0.0750759;
-    //             b2 = 0.96900 * b2 + white * 0.1538520;
-    //             b3 = 0.86650 * b3 + white * 0.3104856;
-    //             b4 = 0.55000 * b4 + white * 0.5329522;
-    //             b5 = -0.7616 * b5 - white * 0.0168980;
-    //             output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
-    //             output[i] *= 0.11; // (roughly) compensate for gain
-    //             b6 = white * 0.115926;
-    //         }
-    //     }
-    //     return node;
-    // })();
 
     const BASE_FREQUENCY = 164.82;
     function detuned(freq: number, centsOffset: number) {
