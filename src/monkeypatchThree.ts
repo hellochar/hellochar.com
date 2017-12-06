2ee476bdbd4ca0b2b0ddfc1a0ae059c709faf609
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-12-05 21:58:09 -0800

    types for BokehShader; throttle things if it's too slow

--- src/monkeypatchThree.ts
+++ src/monkeypatchThree.ts
@@ -2,11 +2,20 @@ import * as THREE from "three";
 
 // HACK monkeypatch the old features that requires THREE on the global namespace
 (window as any).THREE = THREE;
-import "../old/threejs/controls/OrbitControls";
-import "../old/threejs/postprocessing/EffectComposer";
-import "../old/threejs/postprocessing/MaskPass";
-import "../old/threejs/postprocessing/RenderPass";
-import "../old/threejs/postprocessing/ShaderPass";
-import "../old/threejs/shaders/CopyShader";
-import "../old/threejs/shaders/DotScreenShader";
-import "../old/threejs/stats.min";
+// tslint:disable
+
+import "three/examples/js/postprocessing/EffectComposer";
+
+import "three/examples/js/controls/OrbitControls";
+
+import "three/examples/js/libs/stats.min";
+// import * as dat from "three/examples/js/libs/dat.gui.min";
+// (window as any).dat = dat;
+
+import "three/examples/js/postprocessing/BokehPass";
+import "three/examples/js/postprocessing/MaskPass";
+import "three/examples/js/postprocessing/RenderPass";
+import "three/examples/js/postprocessing/ShaderPass";
+import "three/examples/js/shaders/BokehShader";
+import "three/examples/js/shaders/CopyShader";
+import "three/examples/js/shaders/DotScreenShader";
