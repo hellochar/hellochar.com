6ee840065eeb84011ed1c59297a26f1a5d434693
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-12-08 15:57:34 -0800

    put routes in its own file; empty genetics sketch

---
+++ src/routes.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { FullPageSketch } from "./routes/fullPageSketch";
+import { HomePage } from "./routes/homePage";
+
+import {
+    Cymatics,
+    Dots,
+    Flame,
+    Genetics,
+    Line,
+    Waves,
+} from "./sketches";
+
+export const Routes = () => (
+    <Switch>
+        <Route path="/line" component={() => <FullPageSketch sketch={Line} />} />
+        <Route path="/dots" component={() => <FullPageSketch sketch={Dots} />} />
+        <Route path="/waves" component={() => <FullPageSketch sketch={Waves} />} />
+        <Route path="/cymatics" component={() => <FullPageSketch sketch={Cymatics} />} />
+        <Route path="/flame" component={() => <FullPageSketch sketch={Flame} />} />
+        <Route path="/genetics" component={() => <FullPageSketch sketch={Genetics} />} />
+        <Route path="/" component={HomePage} />
+    </Switch>
+);
