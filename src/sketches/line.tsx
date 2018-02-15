b128d9b151620c60f1eb0311a066a00bc8ef14fb
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-02-14 18:42:42 -0800

    bake and optimize - 2x increase trivially!

--- src/sketches/line.tsx
+++ src/sketches/line.tsx
@@ -15,10 +15,14 @@ const NUM_PARTICLES = Number(parse(location.search).p) ||
     (screen.width > 1024 ? 15000 : 5000);
 const SIMULATION_SPEED = 2;
 const GRAVITY_CONSTANT = 280;
-const LIFETIME = 15;
 // speed becomes this percentage of its original speed every second
 const PULLING_DRAG_CONSTANT = 0.93075095702;
 const INERTIAL_DRAG_CONSTANT = 0.53913643334;
+
+// bake it
+const timeStep = .016 * SIMULATION_SPEED;
+const BAKED_PULLING_DRAG_CONSTANT = Math.pow(PULLING_DRAG_CONSTANT, timeStep);
+const BAKED_INERTIAL_DRAG_CONSTANT = Math.pow(INERTIAL_DRAG_CONSTANT, timeStep);
 
 function createAudioGroup(ctx: SketchAudioContext) {
     const backgroundAudio = $("<audio autoplay loop>")
@@ -343,7 +347,6 @@ interface IParticle {
     dx: number;
     dy: number;
     vertex: THREE.Vertex | null;
-    life: number;
 }
 
 function init(_renderer: THREE.WebGLRenderer, _audioContext: SketchAudioContext) {
@@ -369,7 +372,6 @@ function init(_renderer: THREE.WebGLRenderer, _audioContext: SketchAudioContext)
             dx: 0,
             dy: 0,
             vertex: null,
-            life: 0,
         };
         resetToOriginalPosition(particles[i], i);
     }
@@ -396,14 +398,14 @@ function resetToOriginalPosition(particle: IParticle, i: number) {
     particle.x = x;
     particle.y = y;
     particle.dx = particle.dy = 0;
-    particle.life = 0;
 }
 
 let globalFrame = 0;
 
 function animate(millisElapsed: number) {
     const allAttractorPowers = attractors.reduce((b, a) => Math.abs(a.power) + b, 0);
-    dragConstant = (allAttractorPowers > 0.1) ? PULLING_DRAG_CONSTANT : INERTIAL_DRAG_CONSTANT;
+    // dragConstant = (allAttractorPowers > 0.1) ? PULLING_DRAG_CONSTANT : INERTIAL_DRAG_CONSTANT;
+    dragConstant = (allAttractorPowers > 0.1) ? BAKED_PULLING_DRAG_CONSTANT : BAKED_INERTIAL_DRAG_CONSTANT;
 
     attractors.forEach((attractor) => {
         attractor.mesh.position.z = -100;
@@ -417,7 +419,6 @@ function animate(millisElapsed: number) {
 
     gravityShaderPass.uniforms.iMouse.value.set(attractors[0].x, renderer.domElement.height - attractors[0].y);
     // const timeStep = millisElapsed / 1000 * SIMULATION_SPEED;
-    const timeStep = .016 * SIMULATION_SPEED * speedScalar;
     if (returnToStartPower > 0 && returnToStartPower < 1) {
         returnToStartPower *= 1.01;
     }
@@ -427,25 +428,11 @@ function animate(millisElapsed: number) {
     let averageVel2 = 0;
     const nonzeroAttractors = attractors.filter((attractor) => attractor.power !== 0);
 
-    // e.g. out of 100k particles, reset 10k this frame
-    // if globalFrame = 3, reset particles 30k to 40k
-    // const frameOffset = (globalFrame % LIFETIME) / LIFETIME;
-    // for (let i = 0; i < NUM_PARTICLES / LIFETIME; i++) {
-    //     const index = (globalFrame * NUM_PARTICLES / LIFETIME + i) % NUM_PARTICLES;
-    //     const particle = particles[index];
-
-    //     // take this chunk of particles and spread them evenly
-    //     const numParticlesToReset = NUM_PARTICLES / LIFETIME;
-    //     particle.x = map(i + frameOffset, 0, NUM_PARTICLES / LIFETIME, 0, canvas.width);
-    //     particle.y = canvas.height / 2;
-    //     particle.dx = particle.dy = 0;
-    //     particle.life = 0;
-    // }
-
     for (let i = 0; i < NUM_PARTICLES; i++) {
         const particle = particles[i];
 
-        nonzeroAttractors.forEach((attractor) => {
+        for (let j = 0; j < nonzeroAttractors.length; j++) {
+            const attractor = nonzeroAttractors[j];
             const dx = attractor.x - particle.x;
             const dy = attractor.y - particle.y;
             const length2 = Math.sqrt(dx * dx + dy * dy);
@@ -454,23 +441,22 @@ function animate(millisElapsed: number) {
 
             particle.dx += forceX * timeStep;
             particle.dy += forceY * timeStep;
-        });
-        particle.dx *= Math.pow(dragConstant, timeStep);
-        particle.dy *= Math.pow(dragConstant, timeStep);
+        }
+        particle.dx *= dragConstant;
+        particle.dy *= dragConstant;
 
         particle.x += particle.dx * timeStep;
         particle.y += particle.dy * timeStep;
-        particle.life += 1;
         if (particle.x < 0 || particle.x > canvas.width || particle.y < 0 || particle.y > canvas.height) {
             resetToOriginalPosition(particle, i);
         }
 
         const wantedX = i * canvas.width / NUM_PARTICLES;
         const wantedY = canvas.height / 2;
-        if (returnToStartPower > 0) {
-            particle.x -= (particle.x - wantedX) * returnToStartPower;
-            particle.y -= (particle.y - wantedY) * returnToStartPower;
-        }
+        // if (returnToStartPower > 0) {
+        //     particle.x -= (particle.x - wantedX) * returnToStartPower;
+        //     particle.y -= (particle.y - wantedY) * returnToStartPower;
+        // }
 
         particle.vertex!.x = particle.x;
         particle.vertex!.y = particle.y;
@@ -483,7 +469,7 @@ function animate(millisElapsed: number) {
     averageVel2 /= NUM_PARTICLES;
     let varianceX2 = 0;
     let varianceY2 = 0;
-    let varianceVel22 = 0;
+    // let varianceVel22 = 0;
     let entropy = 0;
     let numLeft = 0, numRight = 0;
     for (let i = 0; i < NUM_PARTICLES; i++) {
@@ -492,7 +478,7 @@ function animate(millisElapsed: number) {
             dy2 = Math.pow(particle.y - averageY, 2);
         varianceX2 += dx2;
         varianceY2 += dy2;
-        varianceVel22 += Math.pow(particle.dx * particle.dx + particle.dy * particle.dy - averageVel2, 2);
+        // varianceVel22 += Math.pow(particle.dx * particle.dx + particle.dy * particle.dy - averageVel2, 2);
         const length = Math.sqrt(dx2 + dy2);
         if (length > 0) {
             entropy += length * Math.log(length);
@@ -506,14 +492,14 @@ function animate(millisElapsed: number) {
     entropy /= NUM_PARTICLES;
     varianceX2 /= NUM_PARTICLES;
     varianceY2 /= NUM_PARTICLES;
-    varianceVel22 /= NUM_PARTICLES;
+    // varianceVel22 /= NUM_PARTICLES;
 
     const varianceX = Math.sqrt(varianceX2);
     const varianceY = Math.sqrt(varianceY2);
-    const varianceVel2 = Math.sqrt(varianceVel22);
+    // const varianceVel2 = Math.sqrt(varianceVel22);
 
     const varianceLength = Math.sqrt(varianceX2 + varianceY2);
-    const varianceVel = Math.sqrt(varianceVel2);
+    // const varianceVel = Math.sqrt(varianceVel2);
     const averageVel = Math.sqrt(averageVel2);
 
     // flatRatio = 1 -> perfectly circular
@@ -614,8 +600,6 @@ function mouseup(event: JQuery.Event) {
     }
 }
 
-let speedScalar = 1;
-
 Leap.loop((frame: Leap.Frame) => {
     if (frame.hands.length > 0) {
         instructionsEl.setLastRenderedFrame(globalFrame);
@@ -627,7 +611,6 @@ Leap.loop((frame: Leap.Frame) => {
         attractor.mesh.visible = false;
         attractor.power = 0;
     });
-    speedScalar = 1;
     frame.hands.filter((hand) => hand.valid).forEach((hand, index) => {
         const position = hand.indexFinger.bones[3].center();
 
@@ -645,10 +628,6 @@ Leap.loop((frame: Leap.Frame) => {
         if (hand.indexFinger.extended) {
             // position[2] goes from -300 to 300
             const wantedPower = Math.pow(7, (-position[2] + 350) / 200);
-            // very close to the TV
-            // if (position[2] < -100) {
-            //    speedScalar *= 1 + Math.pow(Math.abs(position[2]) / 100, 2);
-            // }
             attractor.power = attractor.power * 0.5 + wantedPower * 0.5;
         } else {
             attractor.power = attractor.power * 0.5;
