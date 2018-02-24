b14a3c394c196b4aa20e6ad641d4028ef2b3493c
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-02-23 22:50:42 -0800

    modularized styles; add presentation mode

--- src/routes/fullPageSketch.tsx
+++ src/routes/fullPageSketch.tsx
@@ -1,3 +1,5 @@
+import * as classnames from "classnames";
+import { parse } from "query-string";
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import { Link } from "react-router-dom";
@@ -12,8 +14,10 @@ export interface ISketchRouteProps {
 
 export class FullPageSketch extends React.Component<ISketchRouteProps, {}> {
     public render() {
+        const isPresentationMode = !!parse(location.search).presentationMode;
+        const classes = classnames("full-page-sketch", { "presentation-mode": isPresentationMode });
         return (
-            <div className="full-page-sketch">
+            <div className={classes}>
                 <Link className="back-button" to="/">&#10094;</Link>
                 {/* <ShrinkingHeader
                     alwaysShrunken
