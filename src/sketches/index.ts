f43718f391d4180a0a8c40695982cf666bb84747
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-02-23 16:44:12 -0800

    move wip sketches into its own folder

--- src/sketches/index.ts
+++ src/sketches/index.ts
@@ -1,9 +1,5 @@
 export { Cymatics } from "./cymatics";
-export { DontMove } from "./dontmove";
 export { Dots } from "./dots";
 export { Flame } from "./flame";
-export { Genetics } from "./genetics";
 export { Line } from "./line";
 export { Waves } from "./waves";
-export { Waves2 } from "./waves2";
-export { Webcam } from "./webcam";
