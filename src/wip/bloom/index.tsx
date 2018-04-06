a66794e842303d93ab22fc0dbdfaf3a7e2acc3bf
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-04-05 21:56:29 -0700

    abstract component class; generative leaf geometry

--- src/wip/bloom/index.tsx
+++ src/wip/bloom/index.tsx
@@ -3,78 +3,117 @@ import * as THREE from "three";
 
 import { ISketch, SketchAudioContext } from "../../sketch";
 
-class Plant {
-    public constructor(public stem: Stem) {}
+abstract class Component {
+    object?: THREE.Object3D;
 }
 
-class Stem {
-    public constructor(public nodes: Nodes) {}
+class Plant extends Component {
+    public constructor(public stem: Stem) { super(); }
 }
 
-class Nodes {
-    public constructor(public rootNode: Node) {}
+class Stem extends Component {
+    public constructor(public nodes: Nodes) { super(); }
 }
 
-abstract class Node {
-    public constructor(public internode?: Internode) {}
+class Nodes extends Component {
+    public constructor(public rootNode: Node) { super(); }
 }
 
-class Internode {
-    public constructor(public branch: Branch, public node: Node) {}
+abstract class Node extends Component {
+    public constructor(public internode?: Internode) { super(); }
 }
 
-class Branch {}
+class Internode extends Component {
+    public constructor(public branch: Branch, public node: Node) { super(); }
+}
+
+class Branch extends Component {}
 
 // would be nice to emulate both succulents, and flat leaves, broad, thin, curly.
 class Leaves extends Node {
     public constructor(public leaves: Leaf[], internode?: Internode) { super(internode); }
 }
 
-class Leaf {}
+class Leaf extends Component {
+    public object: THREE.Mesh;
+    constructor() {
+        super();
+        // how do you handle leaf curving?
+        const geometry = new THREE.Geometry();
+        for (let i = 0; i < 100; i++) {
+            const theta = i / 100 * Math.PI * 2;
+            const vertex = Leaf.leafPerimeter1(theta);
+            geometry.vertices.push(vertex);
+        }
+        geometry.verticesNeedUpdate = true;
+        for (let i = 2; i < 100; i++) {
+            const face = new THREE.Face3(0, i - 1, i);
+            geometry.faces.push(face);
+        }
+        geometry.computeFaceNormals();
+        geometry.computeVertexNormals();
+        geometry.mergeVertices();
+
+        const material = new THREE.MeshStandardMaterial({
+            color: new THREE.Color("rgb(165, 190, 63)"),
+        })
+        this.object = new THREE.Mesh(
+            geometry,
+            material,
+        );
+    }
+
+    static leafPerimeter1 = (theta: number) => {
+        const r = (3 + 3 * Math.cos(theta) + 1 / (0.1 + Math.pow(Math.sin(theta / 2), 2))) / 16;
+        const x = r * Math.cos(theta);
+        const z = r * Math.sin(theta);
+        return new THREE.Vector3(x, 0, z);
+    }
+}
 
 class Flower extends Node {
     public constructor(public perianth: Perianth, public reproductive: Reproductive) { super(); }
 }
 
-class Perianth {
-    public constructor(public calyx: Calyx, public corolla: Corolla) {}
+class Perianth extends Component {
+    public constructor(public calyx: Calyx, public corolla: Corolla) { super(); }
 }
 
-class Calyx {
-    public constructor(public sepals: Whorl<Sepal>) {}
+class Calyx extends Component {
+    public constructor(public sepals: Whorl<Sepal>) { super(); }
 }
 
 class Whorl<T> {
     public constructor(public elements: T[]) {}
 }
 
-class Sepal {}
+class Sepal extends Component {}
 
-class Corolla {
-    public constructor(public petals: Whorl<Petal>) {}
+class Corolla extends Component {
+    public constructor(public petals: Whorl<Petal>) { super(); }
 }
 
-class Petal {}
+class Petal extends Component {}
 
-class Reproductive {
-    public constructor(public androecium: Androecium, public gynoecium: Gynoecium) {}
+class Reproductive extends Component {
+    public constructor(public androecium: Androecium, public gynoecium: Gynoecium) { super(); }
 }
 
-class Androecium {
-    public constructor(public stamens: Whorl<Stamen>) {}
+class Androecium extends Component {
+    public constructor(public stamens: Whorl<Stamen>) { super(); }
 }
 
-class Stamen {}
+class Stamen extends Component {}
 
-class Gynoecium {
-    public constructor(public pistils: Whorl<Pistil>) {}
+class Gynoecium extends Component {
+    public constructor(public pistils: Whorl<Pistil>) { super(); }
 }
 
-class Pistil {
-    public constructor(public carpels: Carpel[]) {}
+class Pistil extends Component {
+    public constructor(public carpels: Carpel[]) { super(); }
 }
 
-class Carpel {}
+class Carpel extends Component {}
 
 class Bloom extends ISketch {
     public scene = new THREE.Scene();
