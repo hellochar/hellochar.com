2910bc225195a27786efac1448b9d43f18593c1a
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-04-24 14:57:04 -0700

    clean up code in LeafTemplate

--- src/wip/bloom/veinMesh/leafTemplate.ts
+++ src/wip/bloom/veinMesh/leafTemplate.ts
@@ -11,206 +11,19 @@ import { LeafTextureGenerator } from "./textureGen";
  * We use this to share the geometry and material across multiple leaf instances.
  */
 export class LeafTemplate {
-    constructor(
-        public veinedLeaf: VeinedLeaf,
-        public geometry: THREE.Geometry,
-        public material: THREE.MeshBasicMaterial,
-    ) {}
-
-    static fromVeinedLeaf(leaf: VeinedLeaf) {
+    public static fromVeinedLeaf(leaf: VeinedLeaf) {
         // just create a skeleton; we won't bind it
         const skeleton = VeinedLeafSkeleton.createFromVeinedLeaf(leaf);
 
         const geometry = new THREE.Geometry();
-        for (let i = 0; i < skeleton.bones.length; i++) {
-            const leafNode = skeleton.bones[i];
-            const vertex = leafNode.getWorldPosition();
-            geometry.vertices.push(vertex);
-            geometry.skinIndices.push(new THREE.Vector4(i, 0, 0, 0) as any);
-            geometry.skinWeights.push(new THREE.Vector4(1, 0, 0, 0) as any);
-        }
-        geometry.verticesNeedUpdate = true;
-        /* face algorithm
-        for each node, create 4 faces:
-        1. forward, left, me
-        2. forward, me, right
-        3. me, left, parent
-        4. me, parent, right
-         */
-        // const childFaces = () => {
-        //     for (const leafNode of skeleton.bones) {
-        //         const { index, parent, forwardNode, leftNode, rightNode } = leafNode;
-        //         if (forwardNode != null) {
-        //             if (leftNode != null) {
-        //                 geometry.faces.push(new THREE.Face3(forwardNode.index, leftNode.index, index));
-        //                 if (parent instanceof LeafNode) {
-        //                     geometry.faces.push(new THREE.Face3(index, leftNode.index, parent.index));
-        //                 }
-        //             }
-        //             if (rightNode != null) {
-        //                 geometry.faces.push(new THREE.Face3(forwardNode.index, index, rightNode.index));
-        //                 if (parent instanceof LeafNode) {
-        //                     geometry.faces.push(new THREE.Face3(index, parent.index, rightNode.index));
-        //                 }
-        //             }
-        //         }
-        //     }
-        // };
-        // face algorithm 2:
-        // connect siblings in quads
-        // for (const layer of this.sideDepthLayers) {
-        // const cousinFaces = () => {
-        //     for (const layer of skeleton.depthLayers) {
-        //         for (let i = 0; i < layer.length; i++) {
-        //             const me = layer[i];
-        //             const { leftNode, forwardNode, rightNode } = me;
-        //             if (forwardNode != null) {
-        //                 const { leftNode: leftCousin, rightNode: rightCousin } = forwardNode;
-        //                 if (leftNode != null) {
-        //                     geometry.faces.push(new THREE.Face3(me.index, forwardNode.index, leftNode.index));
-        //                     if (leftCousin != null) {
-        //                         geometry.faces.push(new THREE.Face3(leftCousin.index, leftNode.index, forwardNode.index));
-        //                     }
-        //                 }
-        //                 if (rightNode != null) {
-        //                     geometry.faces.push(new THREE.Face3(forwardNode.index, me.index, rightNode.index));
-        //                     if (rightCousin != null) {
-        //                         geometry.faces.push(new THREE.Face3(rightCousin.index, forwardNode.index, rightNode.index));
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-        // };
-        // algorithm 3:
-        // delauney triangulate
+        LeafTemplate.addVerticesAtVeinPositions(geometry, skeleton);
 
-        // return true to include/keep
-        // type Filter = (a: LeafNode, b: LeafNode, c: LeafNode) => boolean;
-        // const triangleFilters: { [name: string]: Filter } = {
-        //     // filter out faces that are all in the edge layer
-        //     // this produces fascinating edge patterns and a few holes
-        //     // TODO doesn't capture the end bones
-        //     noEdgeLayer: (a, b, c) => !(
-        //         skeleton.edgeLayer.indexOf(a) !== -1 &&
-        //         skeleton.edgeLayer.indexOf(b) !== -1 &&
-        //         skeleton.edgeLayer.indexOf(c) !== -1
-        //     ),
-        //     // filter out faces that are in the edge layer and all 3 don't share a parent.
-        //     noEdgeLayerAndSiblings: (a, b, c) => {
-        //         const aParent = a.parent;
-        //         const bParent = b.parent;
-        //         const cParent = c.parent;
-        //         return !(
-        //             skeleton.edgeLayer.indexOf(a) !== -1 &&
-        //             skeleton.edgeLayer.indexOf(b) !== -1 &&
-        //             skeleton.edgeLayer.indexOf(c) !== -1 &&
-        //             aParent !== bParent &&
-        //             bParent !== cParent &&
-        //             aParent !== cParent
-        //         );
-        //     },
-        //     // filter out very thin faces, usually those are indicative of outer edge craziness
-        //     noThinTriangles: (a, b, c) => {
-        //         const aWorld = a.getWorldPosition();
-        //         const bWorld = b.getWorldPosition();
-        //         const cWorld = c.getWorldPosition();
-        //         const aAngle = bWorld.clone().sub(aWorld).angleTo(cWorld.clone().sub(aWorld));
-        //         const bAngle = aWorld.clone().sub(bWorld).angleTo(cWorld.clone().sub(bWorld));
-        //         const cAngle = bWorld.clone().sub(cWorld).angleTo(aWorld.clone().sub(cWorld));
-        //         const epsilonAngle = Math.PI / 180 * 2;
-        //         return triangleFilters.noEdgeLayer(a, b, c) && (
-        //             aAngle > epsilonAngle &&
-        //             bAngle > epsilonAngle &&
-        //             cAngle > epsilonAngle
-        //         );
-        //     },
-        //     noLongTriangles: (a, b, c) => {
-        //         const longDist = 1;
-        //         const aWorld = a.getWorldPosition();
-        //         const bWorld = b.getWorldPosition();
-        //         const cWorld = c.getWorldPosition();
-        //         return aWorld.distanceTo(bWorld) < longDist
-        //             && aWorld.distanceTo(cWorld) < longDist
-        //             && bWorld.distanceTo(cWorld) < longDist;
-        //     },
-        // };
-
-        // const delauneyFaces = (...filters: Filter[]) => {
-        //     const allLeafNodes = skeleton.bones;
-        //     try {
-        //         const delaunator = Delaunator.from(allLeafNodes,
-        //             (bone) => bone.getWorldPosition().x,
-        //             (bone) => bone.getWorldPosition().z,
-        //         );
-        //         // for (let i = 0; i < delaunator.halfedges.length; i++) {
-        //         //     const halfEdgeIndex = delaunator.halfedges[i];
-        //         // }
-        //         for (let i = 0; i < delaunator.triangles.length; i += 3) {
-        //             const indexA = delaunator.triangles[i];
-        //             const indexB = delaunator.triangles[i + 1];
-        //             const indexC = delaunator.triangles[i + 2];
-        //             const a = skeleton.bones[indexA] as LeafNode;
-        //             const b = skeleton.bones[indexB] as LeafNode;
-        //             const c = skeleton.bones[indexC] as LeafNode;
-        //             const passesFilter = filters.every((filter) => filter(a, b, c));
-        //             if (passesFilter) {
-        //                 const face = new THREE.Face3(indexA, indexB, indexC);
-        //                 geometry.faces.push(face);
-        //             }
-        //         }
-        //     } catch (e) {
-        //         console.error(e);
-        //         // do nothing
-        //     }
-        // };
-
-        const alphaHullFaces = () => {
-            // const alpha = 10;
-            const alpha = 1 / skeleton.downScalar / (leaf.growthParameters.DEPTH_STEPS_BEFORE_BRANCHING * 2);
-            const points = skeleton.bones.map((node) => {
-                const {x, z} = node.getWorldPosition();
-                return [x, z];
-            });
-            // this works pretty well, but
-            // it makes no connectedness guarantee. some options:
-            // * detect, reject (and do this whole thing over) unconnected leaves
-            // * in vertex generation, ensure points are no more than e.g. 0.1 units apart
-            //   and then set an appropriate alpha here
-            const cells = alphaComplex(alpha, points);
-            for (let i = 0; i < cells.length; i++) {
-                const [indexA, indexB, indexC] = cells[i];
-                const a = skeleton.bones[indexA] as VeinBone;
-                const b = skeleton.bones[indexB] as VeinBone;
-                const c = skeleton.bones[indexC] as VeinBone;
-                // const passesFilter = filters.every((filter) => filter(a, b, c));
-                // if (passesFilter) {
-                const face = new THREE.Face3(indexA, indexB, indexC);
-                geometry.faces.push(face);
-                // }
-            }
-        };
-
-        // delauneyExceptLastDepthFaces();
-        /* one of two modes
-         * cousin + child builds fan-shaped or compound leaves
-         * delauney builds simple Entire edges
-        */
-        // const mode = (Math.random() < 0.33) ? "compound" : Math.random() < 0.5 ? "entire" : "complexEdge";
-        // const mode = "entire" as any;
-        // const mode = "entire";
-        // if (mode === "compound") {
-        //     childFaces();
-        //     cousinFaces();
-        // } else if (mode === "entire") {
-        //     // delauneyExceptLastDepthFaces();
-        //     // cousinFaces();
-        //     // childFaces();
-        //     delauneyFaces(triangleFilters.noLongTriangles);
-        // } else if (mode === "complexEdge") {
-        //     delauneyFaces(triangleFilters.noEdgeLayerAndSiblings, triangleFilters.noThinTriangles);
-        // }
-        alphaHullFaces();
+        // alpha too high makes the boundary totally convex and isn't interesting
+        // alpha too low puts holes in the leaf where the veins haven't explored into
+        // the depth_steps significantly controls the leaf inner whitespace so we factor it
+        // while choosing alpha
+        const alpha = 1 / skeleton.downScalar / (leaf.growthParameters.DEPTH_STEPS_BEFORE_BRANCHING * 2);
+        LeafTemplate.addAlphaHullFaces(alpha, geometry, skeleton);
 
         geometry.computeFaceNormals();
         geometry.computeVertexNormals();
@@ -239,4 +52,54 @@ export class LeafTemplate {
 
         return new LeafTemplate(leaf, geometry, material);
     }
+
+    /**
+     * Adds one vertex to the geometry for each vein in the VeinedLeafSkeleton.
+     * Also attaches skinIndices/skinWeights corresponding to that bone.
+     */
+    private static addVerticesAtVeinPositions(geometry: THREE.Geometry, skeleton: VeinedLeafSkeleton) {
+        for (let i = 0; i < skeleton.bones.length; i++) {
+            const bone = skeleton.bones[i];
+            const vertex = bone.getWorldPosition();
+            geometry.vertices.push(vertex);
+            geometry.skinIndices.push(new THREE.Vector4(i, 0, 0, 0) as any);
+            geometry.skinWeights.push(new THREE.Vector4(1, 0, 0, 0) as any);
+        }
+        geometry.verticesNeedUpdate = true;
+    }
+
+    /**
+     * Computes the alpha hull on the geometry and adds each triangle as a Face3 to the geometry.
+     */
+    private static addAlphaHullFaces(alpha: number, geometry: THREE.Geometry, skeleton: VeinedLeafSkeleton) {
+        // const alpha = 10;
+        const points = skeleton.bones.map((node) => {
+            const {x, z} = node.getWorldPosition();
+            return [x, z];
+        });
+        // this works pretty well, but
+        // it makes no connectedness guarantee. some options:
+        // * detect, reject (and do this whole thing over) unconnected leaves
+        // * in vertex generation, ensure points are no more than e.g. 0.1 units apart
+        //   and then set an appropriate alpha here
+        const cells = alphaComplex(alpha, points);
+        for (let i = 0; i < cells.length; i++) {
+            const [indexA, indexB, indexC] = cells[i];
+            const a = skeleton.bones[indexA] as VeinBone;
+            const b = skeleton.bones[indexB] as VeinBone;
+            const c = skeleton.bones[indexC] as VeinBone;
+            // const passesFilter = filters.every((filter) => filter(a, b, c));
+            // if (passesFilter) {
+            const face = new THREE.Face3(indexA, indexB, indexC);
+            geometry.faces.push(face);
+            // }
+        }
+    }
+
+    constructor(
+        public veinedLeaf: VeinedLeaf,
+        public geometry: THREE.Geometry,
+        public material: THREE.MeshBasicMaterial,
+    ) {}
+
 }
--- src/wip/bloom/veinMesh/skinnedLeaf.ts
+++ src/wip/bloom/veinMesh/skinnedLeaf.ts
@@ -13,15 +13,6 @@ export class SkinnedLeaf extends THREE.SkinnedMesh {
         this.add(this.skeleton.rootNode);
         this.bind(this.skeleton);
 
-        // const boundingBox = this.geometry.boundingBox;
-        // const xScale = 1 / (boundingBox.max.x - boundingBox.min.x);
-        // // const mesh2 = new THREE.SkinnedMesh(geometry, new THREE.MeshBasicMaterial({color: "darkgreen", transparent: true, opacity: 0.2, wireframe: true, side: THREE.DoubleSide}));
-        // // mesh.add(mesh2);
-        // // mesh2.bind(skeleton);
-
-        // // mesh.scale is used to normalize the leaf x length
-        // this.scale.set(xScale, xScale, xScale);
-
         // hackhack - our faces are on the wrong side i think and shadows aren't showing through
         this.rotation.x = Math.PI;
         this.receiveShadow = true;
