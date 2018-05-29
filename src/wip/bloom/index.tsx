5ea732f41be33d3f5115e41634653bb00e4bedab
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-05-29 16:39:45 -0700

    delete some commented code

--- src/wip/bloom/index.tsx
+++ src/wip/bloom/index.tsx
@@ -34,7 +34,6 @@ class Bloom extends ISketch {
     public composer!: THREE.EffectComposer;
 
     public component?: THREE.Object3D;
-    // public component!: Branch;
 
     private componentBoundingBox: THREE.Box3 = new THREE.Box3();
 
@@ -80,15 +79,9 @@ class Bloom extends ISketch {
             this.peopleMeshes[i] = personMesh;
             personMesh.position.z = -1;
             this.camera.add(personMesh);
-            // this.scene.add(personMesh);
         }
 
         this.initAudio();
-
-        // scene.add(this.person);
-
-        // const bhelper = new THREE.Box3Helper(this.componentBoundingBox);
-        // scene.add(bhelper);
 
     }
 
@@ -136,43 +129,11 @@ class Bloom extends ISketch {
         this.composer = new THREE.EffectComposer(this.renderer);
         this.composer.addPass(new THREE.RenderPass(this.scene, this.camera));
 
-        // this doesn't work too well
-        // const bokehPass = new THREE.BokehPass(this.scene, this.camera, {
-        //     focus: 1,
-        //     aperture: 0.00025,
-        //     maxblur: 0.05,
-        //     // width: this.canvas.width,
-        //     // height: this.canvas.height,
-        // });
-        // this.composer.addPass(bokehPass);
-
-        // const ssaoPass = new THREE.SSAOPass( this.scene, this.camera, this.canvas.width, this.canvas.height );
-        // // ssaoPass.onlyAO = true;
-        // ssaoPass.radius = 8;
-        // ssaoPass.aoClamp = 0.2;
-        // ssaoPass.lumInfluence = 0.6;
-        // this.composer.addPass(ssaoPass);
-
-        // this.renderer.setClearColor(new THREE.Color("rgb("))
-
-        // const saoPass = new THREE.SAOPass( this.scene, this.camera, false, true );
-        // saoPass.params.saoBias = 2.6;
-        // saoPass.params.saoIntensity = 0.30;
-        // saoPass.params.saoScale = 6;
-        // saoPass.params.saoKernelRadius = 4;
-        // saoPass.params.saoMinResolution = 0;
-        // saoPass.params.saoBlur = true;
-        // saoPass.params.saoBlurRadius = 16;
-        // saoPass.params.saoBlurStdDev = 4;
-        // saoPass.params.saoBlurDepthCutoff = 0.05;
-        // saoPass.params.output = THREE.SAOPass.OUTPUT.Default;
-        // this.composer.addPass(saoPass);
-
         this.composer.passes[this.composer.passes.length - 1].renderToScreen = true;
     }
 
     public initComponent() {
-        const branch = new Branch(10);
+        const branch = new Branch(12);
         // const helper = new THREE.SkeletonHelper(branch.meshManager.skeleton.bones[0]);
         // scene.add(helper);
         this.component = branch;
@@ -195,26 +156,6 @@ class Bloom extends ISketch {
         // this.component = leaf;
         // const skeletonHelper = new THREE.SkeletonHelper(leaf.lamina.skeleton.bones[0]);
         // scene.add(skeletonHelper);
-
-        // this.component = new THREE.Object3D();
-        // for (let x = -5; x <= 5; x++) {
-        //     for (let z = -5; z <= 5; z++) {
-        //         randomizeDna();
-        //         const leaf = new Leaf(dna.leafTemplate);
-        //         leaf.position.x = x;
-        //         leaf.position.y = 0.2;
-        //         leaf.position.z = z;
-        //         this.component.add(leaf);
-        //         // leaf.scale.set(0.01, 0.01, 0.01);
-        //         // leaf.skeleton.bones[0].scale.set(0.01, 0.01, 0.01);
-        //         // const helper = new THREE.SkeletonHelper(leaf.skeleton.bones[0]);
-        //         // this.scene.add(helper);
-        //     }
-        // }
-
-        // const flower = Flower.generate();
-        // this.component = flower;
-
     }
 
     private r1: HTMLDivElement | null = null;
