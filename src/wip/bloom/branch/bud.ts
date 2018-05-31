28caeb0f1831b3b991f9e418af25840e39fc995a
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-05-31 14:58:49 -0700

    delete bud sphere

--- src/wip/bloom/branch/bud.ts
+++ src/wip/bloom/branch/bud.ts
@@ -6,25 +6,15 @@ import dna from "../dna";
 export class Bud extends Component {
     public growthPercentage = 0;
     private sprouted = false;
-    public sproutPercentage = 0.25;
-
-    static geom = (() => {
-        const g = new THREE.SphereBufferGeometry(0.06, 5, 7);
-        g.scale(0.8, 1.2, 0.8);
-        return g;
-    })();
-
-    public sphere: THREE.Mesh;
 
     constructor(public eventualSprouts: (bud: Bud) => Component[]) {
         super();
-        this.sphere = new THREE.Mesh(Bud.geom, dna.branchTemplate.material);
-        // this.add(this.sphere);
         this.scale.multiplyScalar(dna.growth.childScalar);
     }
 
     feed(time: number, nutrients: number) {
         this.growthPercentage = Math.min(1, this.growthPercentage + nutrients);
+
         // if (!this.sprouted && this.growthPercentage > dna.growth.budDevelopmentThreshold) {
         const worldScale = this.getWorldScale();
         if (!this.sprouted && worldScale.y > 0.01 && this.growthPercentage > 0.75) {
