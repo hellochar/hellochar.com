0bdc918f2f493d16d9e45263ec7508bd74185953
Xiaohan Zhang <hellocharlien@hotmail.com>
2018-07-10 15:50:47 -0700

    titleMaker

--- src/routes/titleMaker.tsx
+++ src/routes/titleMaker.tsx
@@ -4,9 +4,11 @@ export class TitleMaker extends React.PureComponent<{}, {}> {
     render() {
         return (
             <div id="titleMaker">
+                <link href="https://fonts.googleapis.com/css?family=Dosis|Share" rel="stylesheet" />
                 <div className="titleMaker-container">
-                    <h1 className="titleMaker-title">Cymatics</h1>
-                    <div className="titleMaker-line" />
+                    <h1 className="titleMaker-title">Xiaohan Zhang</h1>
+                    {/* <div className="titleMaker-line" /> */}
+                    <h1 className="titleMaker-secondary">hellochar.com</h1>
                 </div>
             </div>
         );
